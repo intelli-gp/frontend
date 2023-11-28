411f5a56b9a665379e3cd48e61b80c8d1f534524
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2023-11-28 21:22:31 +0200

    Modified the libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import Button from './components/Button'
+import MobileInput from './components/RegionInput'
+// Initialization for ES Users
+import {
+  Datepicker,
+  Input,
+  initTE,
+} from "tw-elements";
+
+initTE({ Datepicker, Input });
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Button
+       
+       >
+         Create
+       </Button>
+        <Button
+        select="primary500"
+        styles={'hover:bg-pink-700'}
+        >
+          Create
+        </Button>
+     <MobileInput/>
+  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
