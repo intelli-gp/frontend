63d565b467478705a6d38e00edae36b30374900b
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-11-29 23:20:39 +0200

    feat: add primitive home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function HomePage() {
+    return (
+        <nav className='flex gap-4 p-4 '>
+            <Link to="/auth/login">Login Page</Link>
+            <Link to="/auth/signup">Sign up Page</Link>
+        </nav>
+    );
+}
