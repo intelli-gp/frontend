01368173006cd5dfea43ffe1cd46ede864dc5aa8
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-11-29 09:48:58 +0200

    style: add .prettierrc file and format the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import './App.css'
-import Button from './components/Button'
-import MobileInput from './components/RegionInput'
+import './App.css';
+import Button from './components/Button';
+import MobileInput from './components/RegionInput';
 
 import DateInput from './components/DateInput';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-      <Button
-       
-       >
-         Create
-       </Button>
-        <Button
-        select="primary500"
-        styles={'hover:bg-pink-700'}
-        >
-          Create
-        </Button>
-     <MobileInput/>
-  <DateInput/>
-      </div>
-    </>
-  )
+    return (
+        <div>
+            <Button>Create</Button>
+            <Button select="primary500" styles={'hover:bg-pink-700'}>
+                Create
+            </Button>
+            <MobileInput />
+            <DateInput />
+        </div>
+    );
 }
 
-export default App
+export default App;
