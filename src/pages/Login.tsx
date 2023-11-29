4a77a1c78a5ea27f5c02d5633b3ac6bb00b6b44c
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-11-29 23:54:31 +0200

    refactor: login page to use the new input component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 
 import Button from '../components/Button';
+import Input from '../components/Input';
 
 export default function LoginPage() {
     const [email, setEmail] = useState<string>('');
@@ -13,49 +14,30 @@ export default function LoginPage() {
 
     // const handleGoogleLoginClick = () => {};
 
-    const onEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(event.target.value);
-    };
-
-    const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    };
-
-    const onRememberMeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRememberMe(event.target.checked);
-    };
-
     return (
         <form className="flex flex-col gap-4 w-[25rem]">
             <h1 className="text-5xl text-neutral-600 font-black text-center py-10">
                 Welcome Back!
             </h1>
-            <div className="flex flex-col gap-2">
-                <label htmlFor="email" className="font-bold">
-                    Email:
-                </label>
-                <input
-                    className="rounded border border-slate-500 p-2"
-                    type="email"
-                    placeholder="email"
-                    id="email"
-                    value={email}
-                    onChange={onEmailChange}
-                />
-            </div>
-            <div className="flex flex-col gap-2">
-                <label htmlFor="password" className="font-bold">
-                    Password:
-                </label>
-                <input
-                    className="rounded border border-slate-500 p-2"
-                    type="password"
-                    placeholder="password"
-                    id="password"
-                    value={password}
-                    onChange={onPasswordChange}
-                />
-            </div>
+
+            <Input
+                type="email"
+                value={email}
+                label="Email"
+                onChange={(e) => {
+                    setEmail(e.target.value);
+                }}
+            />
+
+            <Input
+                type="password"
+                value={password}
+                label="Password"
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                }}
+            />
+
             <div className="flex justify-between items-center">
                 <div className="flex gap-2">
                     <input
@@ -63,12 +45,15 @@ export default function LoginPage() {
                         type="checkbox"
                         id="rememberMe"
                         checked={rememberMe}
-                        onChange={onRememberMeChange}
+                        onChange={(e) => {
+                            setRememberMe(e.target.checked);
+                        }}
                     />
                     <label htmlFor="rememberMe">Remember me.</label>
                 </div>
                 <Link to="./forget-password">Forget your password?</Link>
             </div>
+
             <Button
                 select="primary700"
                 type="submit"
@@ -76,6 +61,7 @@ export default function LoginPage() {
             >
                 Login
             </Button>
+
             <Button
                 type="submit"
                 className="flex items-center justify-center gap-2 text-lg h-auto py-2 w-full"
@@ -83,6 +69,7 @@ export default function LoginPage() {
                 <FcGoogle />
                 Login with google
             </Button>
+            
             <p className="flex gap-2 justify-center">
                 Don't have an account?
                 <Link to="/auth/signup">Create one</Link>
