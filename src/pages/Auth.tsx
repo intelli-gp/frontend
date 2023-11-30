0c48c846b949dad70ad825d4d62e6e876f19114b
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-11-30 20:57:46 +0200

    feat: update AuthTemplatePage component
    make it responsive with different screen sizes

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -4,23 +4,23 @@ import illustration from '../assets/imgs/login-signup-illustration.svg';
 
 export default function AuthTemplatePage() {
     return (
-        <div className="flex h-screen w-screen">
-            <div className="bg-indigo-900 w-2/5 flex justify-center items-center flex-col py-16">
+        <div className="flex min-h-screen w-full xs:max-lg:flex xs:max-lg:justify-center ">
+            <div className="bg-indigo-900 w-2/5 hidden lg:flex  flex-col xl:justify-center items-center py-16">
                 <div className="w-screen flex justify-center items-center flex-col h-full gap-28">
                     <div className="flex flex-col gap-2">
-                        <h1 className="text-7xl text-white font-black">
+                        <h1 className="3xl:text-8xl xl:text-7xl text-6xl text-white font-black">
                             Think
                             <br />
                             Forward
                         </h1>
-                        <h2 className="text-white text-2xl">
+                        <h2 className="text-white xl:text-2xl text-xl">
                             Education for future-proof career
                         </h2>
                     </div>
                     <img
                         src={illustration}
                         alt="todo: add description"
-                        className="w-96"
+                        className="3xl:w-[28rem] lg:w-96 w-72"
                     />
                 </div>
             </div>
