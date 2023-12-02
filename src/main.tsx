d41a28d9d58554273f18c4d1c49c7f5c93b8bbca
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-02 19:54:21 +0200

    feat: configure redux store
    configure redux store and add login form slice

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
+import {store} from './store'
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <HashRouter>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </HashRouter>
     </React.StrictMode>,
 );
