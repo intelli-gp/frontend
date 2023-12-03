21c99bd211cd5ae162f7482f8f7a0631dba501d4
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-03 23:02:32 +0200

    chore: update store index configuration file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { userApi } from './apis/userApi';
 import { loginFormReducer } from './slices/login-form';
 import { singupFormReducer } from './slices/signup-form';
+import { checkAuthenticated } from './middlewares/checkAuthenticated';
+import { authReducer } from './slices/auth';
 
 export type RootState = ReturnType<typeof store.getState>;
 
@@ -8,9 +12,28 @@ const store = configureStore({
     reducer: {
         'login-form': loginFormReducer,
         'signup-form': singupFormReducer,
+        auth: authReducer,
+        [userApi.reducerPath]: userApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+            .concat(userApi.middleware)
+            .concat(checkAuthenticated);
     },
 });
+
+setupListeners(store.dispatch);
 
 export { store };
 export * from './slices/login-form';
 export * from './slices/signup-form';
+export * from './slices/auth';
+export {
+    useFetchUserQuery,
+    useAddUserMutation,
+    useUpdateUserMutation,
+    useLoginUserMutation,
+} from './apis/userApi';
+
+// for dev purposes
+window.store = store;
