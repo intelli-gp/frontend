ab495f346be343ef4c09b0c8f862a9e6138e0a7c
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-03 13:31:21 +0200

    fix: signup form styling

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -4,9 +4,9 @@ import illustration from '../assets/imgs/login-signup-illustration.svg';
 
 export default function AuthTemplatePage() {
     return (
-        <div className="flex min-h-screen w-full xs:max-lg:flex xs:max-lg:justify-center ">
-            <div className="bg-indigo-900 w-2/5 hidden lg:flex  flex-col xl:justify-center items-center py-4">
-                <div className="w-screen flex justify-center items-center flex-col h-full gap-28">
+        <div className="flex min-h-screen w-full xs:max-lg:flex xs:max-lg:justify-center flex-row-reverse">
+            <div className="bg-indigo-900 w-2/5 hidden lg:flex  lg:flex-col xl:justify-center items-center py-4 fixed h-full left-0">
+                <div className="flex justify-center items-center flex-col h-full gap-28">
                     <div className="flex flex-col gap-2">
                         <h1 className="3xl:text-8xl xl:text-7xl text-6xl text-white font-black">
                             Think
