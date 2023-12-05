b056b86fe6a66394023ac7a56725b3d065ef2a5d
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-05 22:22:21 +0200

    feat: start the authentication logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,8 @@ export default function HomePage() {
     return (
         <nav className="flex gap-4 p-4 ">
             <Link to="/auth/login">Login Page</Link>
-            <Link to="/auth/signup">Sign up Page</Link>
+            <Link to="/auth/signup">Signup Page</Link>
+            <Link to="/secret">Secret</Link>
         </nav>
     );
 }
