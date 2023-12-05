e7581beaa9c68cd5d1036e5568a9d4dba548a613
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-05 20:59:16 +0200

    feat: update scrollbar styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { HashRouter } from 'react-router-dom';
 import { store } from './store';
 import { Provider } from 'react-redux';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
