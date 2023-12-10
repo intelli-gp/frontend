9c8f2545ae1f8caf7988dcaf3e5166959eaa0fef
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-10 20:04:09 +0200

    feat: connect ForgetPasswordPage to backend

--- src/pages/auth/ForgetPassword.tsx
+++ src/pages/auth/ForgetPassword.tsx
@@ -2,16 +2,29 @@ import { useState } from 'react';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { FaEnvelope } from 'react-icons/fa';
+import { useLazyResetPasswordQuery } from '../../store';
+import { Response } from '../../types/response';
 
 export default function ForgetPasswordPage() {
     const [email, setEmail] = useState<string>('');
 
+    const [trigger, { isError, error, isSuccess, isFetching, data }] =
+        useLazyResetPasswordQuery();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        await trigger(email).unwrap();
+    };
+
     return (
-        <form className="flex flex-col gap-4 w-[25rem] py-8">
-            <h1 className="text-5xl text-neutral-600 font-black text-center py-10">
-                Forget password?
+        <form
+            className="flex flex-col gap-4 w-[25rem] py-8"
+            onSubmit={handleSubmit}
+        >
+            <h1 className="text-5xl text-neutral-600 font-black text-center py-8 tracking-tight">
+                Forget password
             </h1>
-            <div className="flex flex-col border rounded-md py-4 px-8 gap-2 border-slate-300">
+            <main className="flex flex-col border rounded-md py-8 px-8 gap-2 border-slate-300">
                 <h2 className="text-2xl font-bold text-neutral-600">
                     Recover your password
                 </h2>
@@ -20,8 +33,9 @@ export default function ForgetPasswordPage() {
                     Don't worry, happens to the best of us.
                 </p>
 
-                <div className="flex flex-col gap-6 mt-8">
+                <div className="flex flex-col gap-6 mt-6">
                     <Input
+                        required
                         type="email"
                         label="Your Email (Must be registered)"
                         value={email}
@@ -31,12 +45,25 @@ export default function ForgetPasswordPage() {
                         className="h-auto text-md text-center font-bold w-full gap-2"
                         type="submit"
                         rounded
+                        loading={isFetching}
                     >
                         <FaEnvelope />
                         Email me a recovery link
                     </Button>
                 </div>
-            </div>
+
+                {isError && (
+                    <p className="text-sm text-red-500 text-center">
+                        {(error as Response).data}
+                    </p>
+                )}
+
+                {isSuccess && (
+                    <p className="text-sm text-green-600 text-center font-bold">
+                        An email has been sent.
+                    </p>
+                )}
+            </main>
         </form>
     );
 }
