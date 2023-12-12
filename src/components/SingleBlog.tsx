6df0f23b0839fb8c96b0032eb68f910ce8302a84
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2023-12-12 21:21:26 +0200

    feat: add two components

--- src/components/SingleBlog.tsx
+++ src/components/SingleBlog.tsx
@@ -6,53 +6,57 @@ export default function SingleBlog() {
         return faker.image.urlLoremFlickr({ category: 'people' });
     }, []);
     return (
-        <div className="w-full duration-200 hover:scale-95">
-            <div className="max-h-52 w-full overflow-hidden rounded-t-xl">
-                <img src={Blogt} alt="blog img" className="w-full" />
-            </div>
-            <div className="rounded-b-xl px-5 pb-5 pt-3 shadow-md shadow-indigo-500/10">
-                <div className="flex flex-col gap-3">
+        <div className="w-[90%] rounded-t-xl duration-200 hover:scale-95 max-h-[500px] shadow-md shadow-indigo-500/10 m-3">
+            <img
+                src={Blogt}
+                alt="blog img"
+                className="w-full overflow-hidden rounded-t-xl "
+            />
+            <div className="px-5 pb-5 rounded-b-xl">
+                <div className="flex flex-col gap-2">
                     <div>
                         <a
                             href="#"
-                            className="mr-2 rounded-lg bg-indigo-100 px-3 py-2 text-sm text-slate-600"
+                            className="mr-2 rounded-2xl bg-indigo-100 px-3 py-2 text-xs text-slate-600"
                         >
                             Development
                         </a>
                         <a
                             href="#"
-                            className="mr-2 rounded-lg bg-indigo-100 px-3 py-2 text-sm text-slate-600"
+                            className="mr-2 rounded-2xl bg-indigo-100 px-3 py-2 text-xs text-slate-600"
                         >
                             Software
                         </a>
                     </div>
                     <div className="flex  justify-between">
-                        <h2 className="pt-3 text-xl capitalize text-slate-800 font-extrabold">
-                            Blog Post Title{' '}
+                        <h2 className="pt-3 text-lg  capitalize text-slate-800 font-bold">
+                            Blog Post Title
                         </h2>
-                        <p className="text-sm font-semibold text-slate-600 pt-5 pr-3">
+                        <p className="text-xs font-semibold text-slate-600 pt-5 pr-3">
                             Oct 13 2024
                         </p>
                     </div>
 
-                    <a href="#" className="block font-medium  text-slate-800">
+                    <a
+                        href="#"
+                        className="block font-medium text-xs md:text-sm text-slate-800"
+                    >
                         Lorem ipsum dolor sit amet consectetur. Leo at amet
-                        gravida viverra...
+                        gravida viverra volutpat egestas condimentum. Placerat
+                        adipiscing turpis nibh massa ornare fames lobortis
+                        accumsan.
                     </a>
-                </div>
-                <hr className="my-4 border-slate-100" />
-                <div className="flex">
-                    <img
-                        src={fakeProfilePic}
-                        alt="user img"
-                        className="mr-3 h-10 w-10 rounded-full object-cover"
-                    />
-                    <p className="text-sm font-semibold capitalize text-slate-600">
-                        Jim Jackson
-                        <span className="block text-xs text-slate-400">
-                            web designer
-                        </span>
-                    </p>
+                    <hr className="my-0 border-slate-100" />
+                    <div className="flex">
+                        <img
+                            src={fakeProfilePic}
+                            alt="user img"
+                            className="mr-3 h-8 w-8 rounded-full object-cover"
+                        />
+                        <p className="text-xs font-semibold capitalize text-slate-600 pt-2">
+                            Jim Jackson
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
