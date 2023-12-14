b2a5e01a967cbeff6b02ef6c8c3bff70250bfff9
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2023-12-14 23:42:23 +0200

    refactor: simplify landing page into components

---
+++ src/pages/landingPage/HeroSection.tsx
@@ -0,0 +1,22 @@
+export default function Hero() {
+    return (
+        <section className="bg-bgColor  h-screen lg:max-h-[100vw] max-h-[60vw]  w-full  flex  justify-center">
+            <div className="flex w-full h-full py-5 justify-center items-center sm:justify-start sm:items-start background">
+                <div className=" flex flex-col  justify-center items-center sm:items-start sm:w-3/6 w-5/6 sm:pl-[4rem] pb-4 pt-[4rem] sm:absolute  lg:top-[18%] xl:left-[10%]">
+                    <h1 className="font-bold  lg:text-4xl sm:text-2xl text-2xl xs:text-3xl text-txt sm:text-left text-center  max-w-[520px] ">
+                        Turn Your Ambition Into
+                        <br />
+                        Success Story.
+                    </h1>
+                    <br />
+                    <p className="text-slate-500 mb-8 max-w-[500px]  lg:text-lg  sm:text-[12px] xs:text-[16px]  sm:text-left text-center">
+                        In our student platform, we designed features to support
+                        and enhance your educational journey, enabling you to
+                        thrive academically. Join us today and unlock your
+                        academic potential.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ src/pages/landingPage/Nav.tsx
@@ -0,0 +1,114 @@
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { CiLogin } from 'react-icons/ci';
+import { useState } from 'react';
+import { IoPersonOutline } from 'react-icons/io5';
+import Button from '../../components/Button';
+
+export default function Nav() {
+    const [navbarOpen, setmenuOpen] = useState(false);
+    const handleNav = () => {
+        setmenuOpen(!navbarOpen);
+    };
+    const className = classNames(
+        'fixed right-0 top-0 w-[55%] h-screen md:hidden ease-in bg-indigo-50 py-6 shadow-lg shadow-indigo-400/5 ',
+        {
+            '': navbarOpen,
+            hidden: !navbarOpen,
+        },
+    );
+    const menuItems = ['Home', 'About', 'Features', 'Pricing'];
+
+    return (
+        <nav className=" sticky left-0 top-0 z-50  h-[53px]  bg-txt w-full backdrop-blur">
+            <div className="mx-auto max-w-7xl px-6 2xl:px-16 flex justify-between items-center h-full flex-wrap">
+                <div className="w-1/6">
+                    <div className="w-2/6">
+                        <a href="#">
+                            <img src="" alt="logo" className="w-full" />
+                        </a>
+                    </div>
+                </div>
+                <div className="hidden md:flex md:w-3/6 w-0 ">
+                    <ul className="flex px-1 ">
+                        {menuItems.map((item) => (
+                            <li>
+                                <a
+                                    href="#"
+                                    className="text-sm font-medium text-white lg:text-base ml-10"
+                                >
+                                    {item}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="flex w-2/6 pt-2 md:justify-center  justify-end">
+                    <Link to="/auth/signup" className="md:flex hidden">
+                        <Button
+                            select="primary"
+                            type="button"
+                            className="flex items-center justify-center text-sm h-auto py-[4px] w-5/6 border-2 border-indigo-900 lg:mx-5 rounded-md"
+                        >
+                            Signup
+                        </Button>
+                    </Link>
+                    <Link to="/auth/login" className="md:flex hidden">
+                        <Button
+                            type="button"
+                            outline={true}
+                            className="flex items-center justify-center text-sm h-auto py-[4px] w-5/6 lg:mx-5 rounded-lg"
+                        >
+                            Login
+                        </Button>
+                    </Link>
+                    <div
+                        onClick={handleNav}
+                        id="navbarToggler"
+                        className="md:hidden"
+                    >
+                        <AiOutlineMenu size={25} color="#fff" />
+                    </div>
+                </div>
+            </div>
+            <div className={className}>
+                <div className="flex w-full items-center justify-end px-6">
+                    <div onClick={handleNav} className="cursor-pointer">
+                        <AiOutlineClose size={25} />
+                    </div>
+                </div>
+                <ul className="flex flex-col w-full font-medium mt-4  ">
+                    {menuItems.map((item) => (
+                        <li>
+                            <a
+                                href="#"
+                                className="block border-b-[1px] border-slate-200 py-2 px-10 hover:bg-indigo-900 hover:text-white text-txt"
+                            >
+                                {item}
+                            </a>
+                        </li>
+                    ))}
+                    <li>
+                        <Link
+                            to="/auth/signup"
+                            className="block py-2   border-b-[1px] border-slate-200  px-10 hover:bg-indigo-900 hover:text-white text-txt flex items-center gap-2"
+                        >
+                            <IoPersonOutline size={14} />
+                            Sign up
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/auth/login"
+                            className="block py-2   border-b-[1px] border-slate-200 px-10 hover:bg-indigo-900 hover:text-white text-txt flex items-center gap-2"
+                        >
+                            <CiLogin size={15} />
+                            Log in
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
