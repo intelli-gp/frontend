70076703e69700bc187d65614662f549f0e3a1be
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-23 16:32:03 +0200

    feat: add new 2 toasts
    add warning and info toasts

--- src/utils/toasts.ts
+++ src/utils/toasts.ts
@@ -1,17 +1,41 @@
-import { toast } from 'react-hot-toast';
+import { ToastPosition, toast } from 'react-hot-toast';
 
-export const successToast = (message: string) =>
+export const successToast = (message: string, position = 'bottom-right') =>
     toast.success(message, {
         iconTheme: {
             primary: '#312e81',
             secondary: '#fff',
         },
+        position: position as ToastPosition,
     });
 
-export const errorToast = (message: string) =>
+export const errorToast = (message: string, position = 'bottom-right') =>
     toast.error(message, {
         iconTheme: {
             primary: '#b91c1c',
             secondary: '#fff',
         },
+        position: position as ToastPosition,
     });
+
+export const warningToast = (message: string, position = 'bottom-right') => {
+    toast(message, {
+        icon: '⚠️',
+        iconTheme: {
+            primary: 'yellow',
+            secondary: '#fff',
+        },
+        position: position as ToastPosition,
+    });
+};
+
+export const infoToast = (message: string, position = 'bottom-right') => {
+    toast(message, {
+        icon: '❕',
+        iconTheme: {
+            primary: 'grey',
+            secondary: '#fff',
+        },
+        position: position as ToastPosition,
+    });
+};
