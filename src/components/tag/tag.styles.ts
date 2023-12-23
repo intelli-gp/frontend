a485af4e09aff8be590ea9ef144e8499aadf94f4
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-24 00:39:14 +0200

    refactor: remove Page component from tag.styles

--- src/components/tag/tag.styles.ts
+++ src/components/tag/tag.styles.ts
@@ -39,20 +39,3 @@ export const DeleteTagButton = styled.button`
         background-color: #e0e7ff;
     }
 `;
-
-export const Page = styled.div`
-    width: 100%;
-    min-height: 100vh;
-    padding: 4rem;
-    position: relative;
-    display: flex;
-    gap: 3rem;
-    flex-direction: column;
-
-    @media (max-width: 768px) {
-        padding: 4rem 2rem;
-    }
-    @media (max-width: 426px) {
-        padding: 4rem 1rem;
-    }
-`;
