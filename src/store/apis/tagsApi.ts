90792be42695a2dd8131b62de17a969cb22743b2
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-23 23:52:47 +0200

    feat: create tagsApi

---
+++ src/store/apis/tagsApi.ts
@@ -0,0 +1,22 @@
+import { appApi } from '.';
+import { Response } from '../../types/response';
+
+export const tagsApi = appApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllTags: builder.query<Response, undefined>({
+            query: () => ({
+                url: '/tags',
+                method: 'GET',
+            }),
+        }),
+        getSuggestedTags: builder.query<Response, number>({
+            query: (limit: number) => ({
+                url: '/tags/suggested',
+                method: 'GET',
+                params: { limit },
+            }),
+        }),
+    }),
+});
+
+export const { useGetAllTagsQuery, useGetSuggestedTagsQuery } = tagsApi;
