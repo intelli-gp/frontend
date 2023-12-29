a4d3c9421c863d6b2098ea3fc4f1c4a0365f9a1d
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2023-12-29 23:30:31 +0200

    feat: add style components to StudyPlanner

---
+++ src/pages/study-planner/study-planner.styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+
+export const ModalContent = styled.div`
+    display: flex;
+    height: 500px;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: left;
+    line-height: 1.8;
+    padding:1.6rem;
+    color: #141414;
+`;
+
+export const TaskBoxContainer =styled.div`
+border-color:#0369A1;
+background:#DBEAF2;
+height:100%;
+min-height:50px;
+display:flex;
+flex-direction:column;
+justify-content: space-between;
+align-items: left;
+width:98%;
+border-style: solid;
+border-width:0 0 0 10px;
+padding:10px;
+border-radius:6px;
+`
+export const TasksContainer =styled.div`
+=  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  border: 1px solid;
+  min-height: 160px;
+  border-radius: 0.375rem;
+  padding: 0.5rem;
+`
