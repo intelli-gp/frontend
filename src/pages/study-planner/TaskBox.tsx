70c1fba82605701108af47ff3e2a36528d1e233c
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2023-12-31 07:57:19 +0200

    feat: add more attributes to TaskBox component

--- src/pages/study-planner/TaskBox.tsx
+++ src/pages/study-planner/TaskBox.tsx
@@ -6,23 +6,33 @@ interface TaskProps {
     children?: React.ReactNode;
     courseName?: string;
     taskTitle?: string;
+    description?: string;
+    due_date?: string;
 }
 
 export default function TaskBox({
     children,
     courseName,
     taskTitle,
+    description,
+    due_date,
 }: TaskProps): JSX.Element {
     return (
         <TaskBoxContainer>
             {children}
-            <div>
+            <div className="flex flex-col justify-between items-left">
                 <p className="text-xs text-[#0369A1]">
                     {courseName}{' '}
                     <span className="text-xs text-[#0369A1] opacity-50">
                         {taskTitle}
                     </span>
                 </p>
+                <p className="text-sm pt-[2px] text-[#0369A1]">{description}</p>
+                <div className="pt-6">
+                    <p className="text-[12px] pt-[2px] text-[#0369A1] opacity-50">
+                        {due_date}
+                    </p>
+                </div>
             </div>
         </TaskBoxContainer>
     );
