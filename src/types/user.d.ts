e3af62016167ef79bb670ccc5301924391d5d71e
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-01-10 14:37:01 +0200

    feat: update User type and add UserToSend type

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,10 +1,32 @@
 export type User = {
-    id: number;
-    name: string;
+    id: string;
+    full_name: string;
+    username: string;
     email: string;
+    dob: string;
+    level_id: number;
+    plan_id: number;
+    bio: string;
+    phone_number: string;
+    cover_image: string;
+    image: string;
+    user_tag: string[];
+};
+
+export type UserToSend = {
+    id: string;
+    username: string;
+    fullName: string;
+    email: string;
+    bio: string;
+    dob: string;
+    coverImage: string;
+    phoneNumber: string;
+    image: string;
+    addedInterests: string[];
+    removedInterests: string[];
     password: string;
-    imageUrl: string;
-} & Record<string, any>;
+};
 
 export type UserCredentials = {
     email: string;
