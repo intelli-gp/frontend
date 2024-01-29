b40cab66c4d64b1b8fa599a5826f3ba180146121
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-01-29 15:05:16 +0200

    fix: authSlice order of operations bug

--- src/store/slices/auth.slice.ts
+++ src/store/slices/auth.slice.ts
@@ -18,18 +18,18 @@ const authSlice = createSlice({
     reducers: {
         setCredentials(state, action: PayloadAction<StoredCredentials>) {
             const { user, token } = action.payload;
-            window.localStorage.setItem('token', token);
-            window.localStorage.setItem('user', JSON.stringify(user));
             state.token = token;
             state.user = user;
             state.isAuthenticated = true;
+            window.localStorage.setItem('token', token);
+            window.localStorage.setItem('user', JSON.stringify(user));
         },
         clearCredentials(state) {
-            window.localStorage.removeItem('token');
-            window.localStorage.removeItem('user');
             state.token = '';
             state.user = {};
             state.isAuthenticated = false;
+            window.localStorage.removeItem('token');
+            window.localStorage.removeItem('user');
         },
     },
 });
