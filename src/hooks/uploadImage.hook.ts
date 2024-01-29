709a229bb6486d90bf5d040c2ab51af0cc024e16
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-01-29 15:06:21 +0200

    feat: create custom hook useUploadImage
    useUpload image is a hook used to upload images to cloudinary cloud storage

---
+++ src/hooks/uploadImage.hook.ts
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export const useUploadImage = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const reset = () => {
+        setIsLoading(false);
+    };
+
+    const trigger = async (image: string | Blob) => {
+        const formData = new FormData();
+        formData.append('file', image);
+        formData.append('upload_preset', 'Mujedd');
+        setIsLoading(true);
+        try {
+            let response: any = await fetch(
+                `https://api.cloudinary.com/v1_1/${
+                    import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+                }/image/upload`,
+                {
+                    method: 'POST',
+                    body: formData,
+                },
+            );
+            response = await response.json();
+            setIsLoading(false);
+            return response.url;
+        } catch (err) {
+            throw new Error(err as string);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { isLoading, reset, trigger };
+};
