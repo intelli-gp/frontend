7c3584ae3325e8eebcdf0a9e6153ee209df0be01
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-02-03 20:28:51 +0200

    fix: editModal bug

--- src/pages/study-planner/EditTaskModal.tsx
+++ src/pages/study-planner/EditTaskModal.tsx
@@ -1,136 +1,151 @@
 import { GoDash } from 'react-icons/go';
 import Button from '../../components/Button';
-import Input from '../../components/Input';
-import { useEditTaskMutation,useRemoveTaskMutation } from '../../store';
+import { InputWithLabel } from '../../components/Input';
+import { useEditTaskMutation, useRemoveTaskMutation,useFetchTaskQuery } from '../../store';
 import { Task } from '../../types/event';
 import { ModalContent } from './study-planner.styles';
-import { useEffect, useState } from 'react';
-interface TaskProps {
-    id?:any;
-    title?: string;
-    status?: string;
-    description?: string;
-    due_date?: string ;
-    start?: string;
-    end?: string;
-    color?: string;
-    DueDate?:string;
-    StartDate?:string;
+import { SetStateAction, useEffect, useState } from 'react';
+import { Modal } from '../../components/modal/modal.component';
+import moment from 'moment';
 
-}
 interface ModalProps {
-    task: TaskProps;
+    ID: number;
+    showModal: boolean;
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const EditTaskModal: React.FC<ModalProps> = ({  task }) => {
+export const EditTaskModal: React.FC<ModalProps> = ({ showModal,
+    setShowModal, ID }) => {
+    const { data: getTasks} = useFetchTaskQuery(ID);
+    const task = getTasks?.data || [];
+    console.log(task)
 
-    const id=task.id;
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [color, setColor] = useState('#000ff3');
-    const [due_date, setDueDate] = useState('');
-    const [due_start, setDueStart] = useState('00:00');
-    const [due_end, setDueEnd] = useState('00:00');
-    const [status, setStatus] = useState('');
+    // let task: TaskProps = useFetchTaskQuery(ID);
+    const id = ID;
+    const [title, setTitle] = useState(task?.Title);
+    const [description, setDescription] = useState(task.Description);
+    const [color, setColor] = useState(task?.Color || '#000ff3');
+    const [due_date, setDueDate] = useState(moment(task.DueDate).format().slice(0,10));
+    const [due_start, setDueStart] = useState(moment(task.StartDate).format().slice(11, 16));
+    const [due_end, setDueEnd] = useState(moment(task.DueDate).format().slice(11, 16));
+    const [status, setStatus] = useState(task?.Status || '');
 
     useEffect(() => {
-        setTitle(task?.title ? task.title.replace("|", "") : '');
-        setDescription(task?.description || '');
-        setColor(task?.color || '#000ff3');
-        setDueDate(task?.DueDate ? task?.DueDate.slice(0, 10) : '');
-        setDueStart(task?.StartDate ? task?.StartDate.slice(11, 16) : '');
-        setDueEnd(task?.DueDate ? task?.DueDate.slice(11, 16) : '');
-        setStatus(task?.status || '');
+        setTitle(task?.Title || '');
+        setDescription(task?.Description || '');
+        setColor(task?.Color || '#000ff3');
+        setDueDate(task?.DueDate ? moment(task.DueDate).format().slice(0,10) : '');
+        setDueStart(task?.StartDate ? moment(task.StartDate).format().slice(11, 16) : '');
+        setDueEnd(task?.DueDate ? moment(task.DueDate).format().slice(11, 16) : '');
+        setStatus(task?.Status || '');
     }, [task]);
+
+
+
 
     const [removeTask, t] = useRemoveTaskMutation();
     const [addTask, a] = useEditTaskMutation();
 
-    
-    const handleDelete = ()=> {
-        
+
+    const handleDelete = () => {
+
         const task: Partial<Task> = {
-            id: id,
+            id: ID,
             title: title,
             description: description,
             color: color,
-            due_end:  due_date+'T'+due_end,
+            due_end: due_date + 'T' + due_end,
             due_date: due_date,
-            due_start: due_date+'T'+due_start,
-            status:status,
+            due_start: due_date + 'T' + due_start,
+            status: status,
 
         };
 
         try {
             console.log(t);
             removeTask(task as Task);
-        } catch (err) {
+            setShowModal(false);
+                } catch (err) {
             console.log(err);
+
         }
     };
-    const handleSubmitForm = ()=> {
-        
+    const handleSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
         const task: Partial<Task> = {
             id: id,
             title: title,
             description: description,
             color: color,
-            due_end:  due_date+'T'+due_end,
+            due_end: due_date + 'T' + due_end,
             due_date: due_date,
-            due_start: due_date+'T'+due_start,
-            status:status,
-
+            due_start: due_date + 'T' + due_start,
+            status: status,
         };
-        console.log(a)
+
+        try {
+            console.log(task)
             addTask(task as Task);
+            setShowModal(false);
+        } catch (err) {
+            console.log(err);
+        }
     };
 
+
     return (
-         // TODO: Solve the bug of useref.
-        <div className="flex justify-between h-[100vh]">
-            <>
+            <Modal isOpen={showModal} setIsOpen={setShowModal}>
                 <ModalContent>
                     <h1 className="text-3xl font-semibold text-txt"> Edit Task </h1>
-                    <>
-                    <div className="w-full">
-                                <Input
-                                    required
-                                    label="Task name"
-                                    type="text"
-                                    value={title}
-                                    onChange={(e) => setTitle(e.target.value)}
-                                />
-                            </div>
+                    <form onSubmit={handleSubmitForm}>
+                        <div className="w-full">
+                            <InputWithLabel
+                                required
+                                label="Task name"
+                                type="text"
+                                value={title}
+                                onChange={(e: { target: { value: SetStateAction<string>; }; }
+                                    ) =>
+                                    setTitle(e.target?.value)
+                                }
+                            />
+                        </div>
                         <div className="flex w-full justify-between pt-[6px] gap-6">
                             <div className="w-1/2">
-                                <Input
+                                <InputWithLabel
                                     required
                                     label="Status"
                                     type="text"
                                     value={status}
-                                    onChange={(e) => setStatus(e.target.value)}
+                                    onChange={(
+                                        e: { target: { value: SetStateAction<string>; }; }
+                                    ) => setStatus(e.target.value)}
                                 />
                             </div>
                             <div className="w-1/2">
-                                <Input
+                                <InputWithLabel
                                     required
                                     label="Select color"
                                     id="color"
                                     className="rounded border  border-slate-400 p-2 w-full h-[49px] bg-white focus-visible:outline-indigo-700 focus-visible:outline-2 focus-visible:outline-offset-2"
                                     type="color"
                                     value={color}
-                                    onChange={(e) => setColor(e.target.value)}
+                                    onChange={(
+                                        e: { target: { value: SetStateAction<string>; }; }
+                                    ) => setColor(e.target.value)}
                                 />
                             </div>
                         </div>
                         <div className="flex w-full justify-between pt-[6px] gap-6">
                             <div className="w-1/2 flex flex-col justify-between">
-                                <Input
+                                <InputWithLabel
                                     required
                                     value={due_date}
                                     type="date"
                                     label="Due date"
-                                    onChange={(e) => setDueDate(e.target.value)}
+                                    onChange={(
+                                        e: { target: { value: SetStateAction<string>; }; }
+                                    ) => setDueDate(e.target.value)}
                                 />
                             </div>
                             <div className="flex flex-row justify-between pt-9 w-1/2">
@@ -138,7 +153,9 @@ export const EditTaskModal: React.FC<ModalProps> = ({  task }) => {
                                     type="time"
                                     className="rounded border border-slate-400 p-2 min-w-0 focus-visible:outline-indigo-700 focus-visible:outline-2 focus-visible:outline-offset-2 mr-1"
                                     value={due_start}
-                                    onChange={(e) => setDueStart(e.target.value)}
+                                    onChange={(e) =>
+                                        setDueStart(e.target.value)
+                                    }
                                 />
                                 <span className="text-xl pt-3">
                                     <GoDash />
@@ -167,7 +184,7 @@ export const EditTaskModal: React.FC<ModalProps> = ({  task }) => {
                             />
                         </div>
                         <div className="w-full flex flex-row gap-4 justify-end items-end pt-5">
-                           <Button type="button" select="primary" className="w-[25%] border-2 border-indigo-900 " onClick={handleSubmitForm}>
+                            <Button type="submit" select="primary" className="w-[25%] border-2 border-indigo-900 ">
                                 Save
                             </Button>
                             <Button type="button" select='danger' outline={true} onClick={handleDelete} className="w-[25%]">
@@ -175,9 +192,8 @@ export const EditTaskModal: React.FC<ModalProps> = ({  task }) => {
                             </Button>
 
                         </div>
-                    </>
+                    </form>
                 </ModalContent>
-            </>
-        </div>
+            </Modal>
     )
 };
