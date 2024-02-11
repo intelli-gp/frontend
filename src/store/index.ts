714b8a9d71787ead40b668bd69f831e15a4fd062
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-02-11 12:14:23 +0200

    refactor: eliminate redundant slice operation

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,6 @@ import { appApi } from './apis/appApi';
 import { authReducer } from './slices/auth.slice';
 import { loginFormReducer } from './slices/login-form.slice';
 import { singupFormReducer } from './slices/signup-form.slice';
-import { taskFormReducer } from './slices/task-form.slice';
 import { articleCreatorReducer } from './slices/article-creator.slice';
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -14,7 +13,6 @@ const store = configureStore({
     reducer: {
         'login-form': loginFormReducer,
         'signup-form': singupFormReducer,
-        'task-form': taskFormReducer,
         'article-creator': articleCreatorReducer,
         auth: authReducer,
         [appApi.reducerPath]: appApi.reducer,
@@ -29,7 +27,6 @@ setupListeners(store.dispatch);
 export { store };
 export * from './slices/login-form.slice';
 export * from './slices/signup-form.slice';
-export * from './slices/task-form.slice';
 export * from './slices/auth.slice';
 export * from './slices/article-creator.slice'
 export * from './apis';
