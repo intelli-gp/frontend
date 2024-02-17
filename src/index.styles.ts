9e4ac5e7d1604cfa411b5188d4fc99b08bed8031
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-02-17 21:17:09 +0200

    feat: add global styled components
    to be used in all common places around the project

---
+++ src/index.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const PageTitle = styled.h1`
+    font-weight: 900;
+    font-size: 2.5rem;
+    color: var(--gray-800);
+`;
+
+export const ModalTitle = styled.h2`
+    font-weight: 900;
+    font-size: 2.25rem;
+    color: var(--gray-800);
+`;
