2cd8d35363653194c6bf4f36aa5e8b47af4243d6
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-02-17 21:14:57 +0200

    fix: update modal component
    handle overflow condition

--- src/components/modal/model.styles.ts
+++ src/components/modal/model.styles.ts
@@ -14,6 +14,8 @@ export const Background = styled.div`
 
 export const ModalWrapper = styled.div`
     width: 600px;
+    max-height: 90vh;
+    overflow-y: auto;
     box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
     background: #fff;
     color: #000;
@@ -21,5 +23,22 @@ export const ModalWrapper = styled.div`
     padding: 2rem 3.5rem;
     @media (max-width: 500px) {
         margin: 5rem 0rem 0rem 0rem;
+    }
+    /* width */
+    &::-webkit-scrollbar {
+        width: 0.5rem;
+    }
+
+    /* Track */
+    &::-webkit-scrollbar-track {
+        width: 0.5rem;
+        background: transparent;
+    }
+
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+        border-radius: 10px;
+        background: var(--indigo-900);
+        opacity: 0.2;
     }
 `;
