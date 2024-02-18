e37ac4005ba951f5ecace33db386c65e27186ea4
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-02-18 17:05:06 +0200

    feat: update article api
    update article api to invalidate cache on mutations

--- src/store/apis/articlesApi.ts
+++ src/store/apis/articlesApi.ts
@@ -4,6 +4,7 @@ import { ArticleToSend } from '../../types/article.d';
 const articleApi = appApi.injectEndpoints({
     endpoints: (builder) => ({
         createArticle: builder.mutation<Response, ArticleToSend>({
+            invalidatesTags: ['Article'],
             query: (article) => ({
                 url: '/articles',
                 method: 'POST',
@@ -11,12 +12,14 @@ const articleApi = appApi.injectEndpoints({
             }),
         }),
         getArticles: builder.query<Response, void>({
+            providesTags: ['Article'],
             query: () => ({
                 url: '/articles',
                 method: 'GET',
             }),
         }),
         getArticle: builder.query<Response, number>({
+            providesTags: (_result, _error, id) => [{ type: 'Article', id }],
             query: (id) => ({
                 url: `/articles/${id}`,
                 method: 'GET',
