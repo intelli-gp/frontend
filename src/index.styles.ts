ff33751f2fb5b96d33da4921105514ca9757a568
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-02-19 23:50:24 +0200

    feat: update styling of PageTitle

--- src/index.styles.ts
+++ src/index.styles.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
 export const PageTitle = styled.h1`
+    line-height: 1.25;
     font-weight: 900;
     font-size: 3rem;
     color: var(--gray-800);
+
+    @media (max-width: 768px) {
+        line-height: 0.9;
+    }
 `;
 
 export const ModalTitle = styled.h2`
