7694f2b98d195c9aa865e6ea6bc0621d0a6c32a2
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-03-03 09:42:19 +0200

    feat: create AI helper page

---
+++ src/pages/AI-helper/AI-helper.page.tsx
@@ -0,0 +1,90 @@
+import { IoSend } from "react-icons/io5";
+import Button from "../../components/Button";
+import { InputWithoutLabel } from "../../components/Input";
+import { AIicon, ChatBody, ChatFooter, ChatHeader, Messagebar, PageContainer, ChatBox } from "./AI-helper.style";
+import AIimg from '../../assets/imgs/AI-profile.svg'
+import { Link } from "react-router-dom";
+
+type MessageType = {
+    type: string;
+    message: string;
+    incoming: boolean;
+    date: string;
+};
+
+const TextMsg = ({ el }: { el: MessageType }) => {
+    return (
+        <ChatBox incoming={el.incoming}>
+            <div>
+                <p>{el.message}</p>
+                <span>{el.date}</span>
+            </div>
+        </ChatBox>
+    );
+};
+
+
+const AIHelperPage = () => {
+
+
+    const data1 = [
+        {
+            type: 'msg',
+            message: 'HI, how are you?',
+            incoming: true,
+            date: '10/16/2023, 10:51:23 AM ',
+        },
+        {
+            type: 'msg',
+            message: 'Thanks for asking! I am fine and u?',
+            incoming: false,
+            date: '10/16/2023, 10:51:23 AM ',
+        },
+        {
+            type: 'msg',
+            message: 'I am great, Can you come to work today? I am great, Can you come to work today? I am great, Can you come to work today?',
+            incoming: true,
+            date: '10/16/2023, 10:51:23 AM ',
+
+        },
+
+    ];
+
+    return (
+        <PageContainer>
+            <ChatHeader>
+                <span className="flex flex-row gap-4 items-center">
+                    <AIicon src={AIimg} />
+                    <h1 className="text-lg font-bold text-[var(--slate-700)]">
+                        AI Helper
+                    </h1>
+                </span>
+                <Link to="/app/upgrade">
+                    <Button type="button" select="secondary" className="text-[#312E81] h-[38px] w-[80px]">
+                        Upgrade
+                    </Button>
+                </Link>
+            </ChatHeader>
+            <div className="flex flex-col md:px-[120px] px-[50px] h-[calc(100vh-70px)]">
+                <ChatBody>
+                    <div />
+                    {data1.map((text) => (
+                        <TextMsg el={text} />
+                    ))}
+                </ChatBody>
+                <ChatFooter>
+                    <Messagebar>
+                        <InputWithoutLabel
+                            className="shadow-none bg-[var(--slate-100)] border-none "
+                            placeholder="Type a message..."
+                        />
+                        <IoSend color="var(--indigo-800)" size={26} />
+                    </Messagebar>
+
+                </ChatFooter>
+            </div>
+
+        </PageContainer>
+    )
+}
+export default AIHelperPage;
---
+++ src/pages/AI-helper/AI-helper.style.ts
@@ -0,0 +1,124 @@
+import styled from 'styled-components';
+import image from '../../assets/imgs/AI-helper.svg'
+
+export const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    background: var(--indigo-50);
+    height: 100vh;
+    background-image: url(${image}); 
+    background-size: cover; 
+    background-repeat: no-repeat; 
+    background-position: center; 
+`;
+
+
+export const ChatHeader = styled.div`
+    width: 100%;
+    height: 70px;
+    background: var(--gray-50);
+    display: flex;
+    flex-direction: row;
+    padding: 20px 55px;
+    justify-content: space-between;
+    align-items: center;
+`;
+export const AIicon = styled.img`
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
+`;
+export const ChatBody = styled.div`
+    width: 100%;
+    padding: 34px;
+    flex-grow: 1;
+    gap: 20px;
+    background:white;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    /* width */
+    &::-webkit-scrollbar {
+        width: 0.5rem;
+    }
+
+    /* Track */
+    &::-webkit-scrollbar-track {
+        width: 0.6rem;
+        background: transparent;
+    }
+
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+        background: var(--gray-300);
+        opacity: 0.2;
+    }
+    & > div:first-child {
+        flex: 1 1 auto;
+        min-height: 6px;
+    }
+`;
+
+export const Messagebar = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+    height:100%;
+    border: 2px solid  var(--gray-500);
+    border-radius: 10px;
+    background:  var(--gray-50);
+    transition: background 0.25s;
+    padding:10px 15px;
+    input {
+
+        background: transparent;
+        outline: none;
+        border: none;
+        flex: 1;
+    }
+    &:focus-within {
+        border: 2px solid #4f46e5;
+    }
+`;
+export const ChatFooter = styled.div`
+    width: 100%;
+    height: 75px;
+    background:  white;
+    padding: 10px 32px 15px 32px;
+`;
+type userType = {
+    incoming: boolean;
+};
+export const ChatBox = styled.div<userType>`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: ${({ incoming }) =>
+        incoming ? 'flex-end' : 'flex-start'};
+
+    & > div:first-child {
+        background-color: ${({ incoming }) =>
+            incoming ? 'var(--indigo-600)' : 'var(  --gray-200)'};
+        padding: ${({ incoming }) => (incoming ? '8px  15px ' : '15px')};
+        border-radius: ${({ incoming }) =>
+            incoming ? '16px 16px 0px 16px' : '16px 16px 16px 0px'};
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        max-width: 40%;
+        min-width:20%;
+
+    }
+
+    p {
+        color: ${({ incoming }) => (incoming ? 'white' : ' var(--gray-800)')};
+    }
+
+    span {
+        display: flex;
+        justify-content: flex-end;
+        font-size: 10px;
+        color: ${({ incoming }) => (incoming ? 'white' : 'var(--slate-500)')};
+    }
+`;
