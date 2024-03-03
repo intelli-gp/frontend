279beacfc9dc4944aaccf6e079c1b36ebfc47388
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-03-03 19:14:19 +0200

    feat(groups): update group types

--- src/types/group.d.ts
+++ src/types/group.d.ts
@@ -1,7 +1,20 @@
+import { User } from './user';
+
 export type ReceivedGroup = {
+    group_id: string;
     title: string;
     description: string;
     cover_image_url: string;
-    group_tag: string[];
-    group_user: string[];
+    GroupTags: string[];
+    GroupMembers: Array<
+        Partial<User> & { joining_status: boolean; type: 'ADMIN' | 'MEMBER' }
+    >;
+    GroupOwner: Partial<User>;
+};
+
+export type GroupToSend = {
+    GroupTitle: string;
+    GroupDescription: string;
+    GroupCoverImageUrl: string;
+    GroupTags: string[];
 };
