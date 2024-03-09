3b8eaa0f5757e78fe452b488cfab6f64beff3b83
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-03-09 20:45:31 +0200

    fix(modal): body overflow hidden bug

--- src/components/modal/modal.component.tsx
+++ src/components/modal/modal.component.tsx
@@ -27,9 +27,10 @@ export const Modal = ({
     useEffect(() => {
         if (isOpen) {
             document.body.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = 'unset';
         }
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
     }, [isOpen]);
 
     return ReactDOM.createPortal(
