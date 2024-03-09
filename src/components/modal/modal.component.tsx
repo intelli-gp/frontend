7732d99edb8730073eb8b627702b46fa1da49901
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-03-09 08:48:04 +0200

    feat: animate modal component

--- src/components/modal/modal.component.tsx
+++ src/components/modal/modal.component.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
@@ -35,9 +36,23 @@ export const Modal = ({
         <>
             {isOpen && (
                 <Background onClick={closeModal} ref={modalRef}>
-                    <ModalWrapper className={className ?? ''}>
-                        {children}
-                    </ModalWrapper>
+                    <motion.div
+                        initial={{
+                            opacity: 0,
+                            y: 100,
+                        }}
+                        animate={{
+                            opacity: 1,
+                            y: 0,
+                        }}
+                        transition={{
+                            duration: 0.15,
+                        }}
+                    >
+                        <ModalWrapper className={className ?? ''}>
+                            {children}
+                        </ModalWrapper>
+                    </motion.div>
                 </Background>
             )}
         </>,
