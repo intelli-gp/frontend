e8834c3743e8e3b47614e1763c54aafa3c269776
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-03-16 02:15:13 +0200

    refactor: Fetch real article data on the home page

---
+++ src/components/article-item/article-item.page.tsx
@@ -0,0 +1,47 @@
+
+import defaultUserImage from '../../assets/imgs/user.jpg';
+import { ReceivedArticle } from '../../types/article';
+import Tag from '../tag/tag.component';
+import { Container, Title, UserImage, Username } from './article-item.style';
+
+const SingleBlog = ({
+    Author: author,
+    CoverImage: coverImageUrl,
+    ArticleTags: tags,
+    Title: title,
+    UpdatedAt: updatedAt,
+}: ReceivedArticle) => {
+
+    return (
+        <Container>
+            <img src={coverImageUrl} alt="blog img" className="w-full rounded-t-xl" />
+            <div className='py-[12px] px-[20px]'>
+                <div className="flex flex-col gap-2">
+                    <div className='flex flex-row p-[8px] gap-2'>
+                        {tags?.slice(0, 3)
+                            .map((tag) => <Tag text={tag} size="sm" />)}
+                    </div>
+                    <div className="flex  justify-between">
+                        <Title>
+                            {title}
+                        </Title>
+                        <p className="text-xs  text-slate-600 pt-2 ">
+                            {new Date(updatedAt).toLocaleDateString('en-US', { day: '2-digit', month: 'short', year: 'numeric' })}
+                        </p>
+                    </div>
+                    <hr className="my-0 border-slate-200" />
+                    <div className="flex items-center ">
+                        <UserImage
+                            src={author?.ProfileImage ?? defaultUserImage}
+                            alt="user img"
+                        />
+                        <Username>
+                        {author.FullName}
+                        </Username>
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
+}
+export default SingleBlog;
---
+++ src/components/article-item/article-item.style.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+padding-top: 0.4rem;
+font-size: 1.2rem;
+text-transform: capitalize;
+color: var(--slate-800);
+font-weight: bold;
+    max-width: 70%;
+    overflow: hidden;
+    white-space: wrap;
+    text-overflow: ellipsis;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  transition: transform 0.2s;
+  border-radius: 0.75rem 0.75rem 0 0;
+  display: flex;
+  flex-direction: column;
+  box-shadow:var(--gray-shadow);
+  margin-bottom: 1.5rem;
+
+  &:hover {
+    transform: scale(0.95);
+  }
+
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  border-radius: 0.75rem 0.75rem 0 0;
+`;
+
+export const UserImage = styled.img`
+  margin-right: 1rem;
+  height: 2.3rem;
+  width: 2.3rem;
+  border-radius:50%;
+  object-fit: cover;
+`;
+
+export const Username = styled.p`
+  font-size: 0.85rem;
+  font-weight: 600;
+  text-transform: capitalize;
+  color: var(--slate-600)
+`;
