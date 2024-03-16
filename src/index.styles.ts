132c11acd520a1d09a8172423b99be5d81510ce5
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-03-16 17:01:42 +0200

    feat: update ModalTitle styled component
    add font sizes to the title component

--- src/index.styles.ts
+++ src/index.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const PageTitle = styled.h1`
     user-select: none;
@@ -12,9 +12,28 @@ export const PageTitle = styled.h1`
     }
 `;
 
-export const ModalTitle = styled.h2`
+export const ModalTitle = styled.h2<{ fontSize?: 'sm' | 'md' | 'lg' | string }>`
     user-select: none;
     font-weight: 700;
-    font-size: 2.5rem;
+    font-size: ${({ fontSize }) => {
+        switch (fontSize) {
+            case 'sm':
+                return '1.5rem';
+            case 'md':
+            case undefined:
+                return '2rem';
+            case 'lg':
+                return '2.5rem';
+            default:
+                return fontSize;
+        }
+    }};
     color: var(--gray-800);
+`;
+
+export const CssTextLengthLimit = css<{ chars?: number; lines?: number }>`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: ${(props) => props.chars || 10}ch;
 `;
