d397740301dcec555ad05afa8b89a9b1c15ee063
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-03-16 23:58:24 +0200

    feat: update message component
    handle deleted messages

--- src/components/message/message.component.tsx
+++ src/components/message/message.component.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
+import { MdDoNotDisturb } from 'react-icons/md';
 import { useSelector } from 'react-redux';
 
 import defaultUserImage from '../../assets/imgs/user.jpg';
@@ -18,6 +19,7 @@ import {
     Message,
     MessageContent,
     MessageDate,
+    MessageHeader,
     OptionsButton,
     SenderName,
     SenderProfile,
@@ -33,6 +35,7 @@ const ChatMessage = ({ message, enableOptions }: ChatMessageProps) => {
 
     const isMine = message.User.ID === user.ID; // Does this message belongs to me.
     const [editMessageIsOpen, setEditMessageIsOpen] = useState(false);
+    const [deleteMessageIsOpen, setDeleteMessageIsOpen] = useState(false);
     const [editMessageText, setEditMessageText] = useState(message.Content);
     const [updateMessage] = useUpdateMessageMutation();
     const [deleteMessage] = useDeleteMessageMutation();
@@ -58,12 +61,14 @@ const ChatMessage = ({ message, enableOptions }: ChatMessageProps) => {
             await deleteMessage({ MessageID: message.MessageID }).unwrap();
         } catch (error) {
             console.log(error);
+        } finally {
+            setDeleteMessageIsOpen(false);
         }
     };
 
     let messageOptions = [
         { option: 'Edit', handler: () => setEditMessageIsOpen(true) },
-        { option: 'Delete', handler: handleDeleteMessage },
+        { option: 'Delete', handler: () => setDeleteMessageIsOpen(true) },
     ];
 
     const UpdateMessageModal = (
@@ -100,12 +105,43 @@ const ChatMessage = ({ message, enableOptions }: ChatMessageProps) => {
         </Modal>
     );
 
+    const DeleteMessageModal = (
+        <Modal
+            isOpen={deleteMessageIsOpen}
+            setIsOpen={setDeleteMessageIsOpen}
+            className="flex flex-col gap-8"
+        >
+            <ModalTitle fontSize="sm">
+                Are you sure you want to delete this message?
+            </ModalTitle>
+            <div className="flex gap-4 flex-row-reverse">
+                <Button
+                    type="button"
+                    outline
+                    select="danger"
+                    onClick={handleDeleteMessage}
+                >
+                    Delete
+                </Button>
+                <Button
+                    type="button"
+                    onClick={() => {
+                        setDeleteMessageIsOpen(false);
+                    }}
+                >
+                    Cancel
+                </Button>
+            </div>
+        </Modal>
+    );
+
     return (
         <Message isMine={isMine}>
             {editMessageIsOpen && UpdateMessageModal}
-            <div className="flex gap-2 items-center">
+            {deleteMessageIsOpen && DeleteMessageModal}
+
+            <MessageHeader isMine={isMine}>
                 <SenderProfile
-                    isMine={isMine}
                     alt="sender profile image"
                     src={message.User.ProfileImage ?? defaultUserImage}
                 />
@@ -116,21 +152,31 @@ const ChatMessage = ({ message, enableOptions }: ChatMessageProps) => {
                 >
                     {message.User.FullName}
                 </SenderName>
-                {isMine && enableOptions && (
-                    <DropdownMenu
-                        options={messageOptions}
-                        mainElementClassName="!absolute top-1 right-1"
-                        right={'100%'}
-                        left={'auto'}
-                        menuWidth="8rem"
+            </MessageHeader>
+
+            {isMine && enableOptions && !message.IsDeleted && (
+                <DropdownMenu
+                    options={messageOptions}
+                    mainElementClassName={`!absolute top-0 right-0`}
+                    right='100%'
+                    bottom='120%'
+                    left='auto'
+                    menuWidth="8rem"
+                >
+                    <OptionsButton
+                        title={'Options'}
+                        className={'options-button'}
                     >
-                        <OptionsButton title={'Options'}>
-                            <IoIosArrowDown size={14} />
-                        </OptionsButton>
-                    </DropdownMenu>
-                )}
-            </div>
-            <MessageContent isMine={isMine}>{message.Content}</MessageContent>
+                        <IoIosArrowDown size={18} />
+                    </OptionsButton>
+                </DropdownMenu>
+            )}
+
+            <MessageContent isMine={isMine} isDeleted={message.IsDeleted}>
+                {message.IsDeleted && <MdDoNotDisturb size={18} />}{' '}
+                {message.Content}
+            </MessageContent>
+
             <MessageDate isMine={isMine}>
                 {new Date(message.CreatedAt ?? Date.now()).toLocaleString()}
             </MessageDate>
--- src/components/message/message.style.ts
+++ src/components/message/message.style.ts
@@ -7,19 +7,34 @@ export const Message = styled.div<{ isMine: boolean }>`
     align-self: ${({ isMine }) => (isMine ? 'flex-end' : 'flex-start')};
     background-color: ${({ isMine }) =>
         isMine ? 'var(--indigo-700)' : 'var(--gray-200)'};
-    padding: 0.5rem 1rem;
+    padding: 1rem;
     border-radius: ${({ isMine }) =>
         isMine ? '16px 16px 0px 16px' : '16px 16px 16px 0px'};
     display: flex;
     flex-direction: column;
     max-width: 70%;
     min-width: 20%;
-    gap: 1rem;
+    gap: 0.5rem;
     position: relative;
+
+    .options-button {
+        transition: opacity 0.2s ease-in-out;
+        opacity: 0;
+    }
+    &:hover {
+        .options-button {
+            opacity: 1;
+        }
+    }
 `;
 
-export const SenderProfile = styled.img<{ isMine: boolean }>`
-    display: ${({ isMine }) => (isMine ? 'none' : '')};
+export const MessageHeader = styled.header<{ isMine: boolean }>`
+    display: ${({ isMine }) => (isMine ? 'none' : 'flex')};
+    gap: 0.5rem;
+    align-items: center;
+`;
+
+export const SenderProfile = styled.img`
     width: 24px;
     height: 24px;
     border-radius: 50%;
@@ -34,32 +49,32 @@ export const SenderName = styled.h2<{ isMine: boolean; chars: number }>`
     font-size: 0.75rem;
 `;
 
-export const MessageContent = styled.p<{ isMine?: boolean }>`
-    margin-top: 0.5rem;
-    font-size: 1rem;
+export const MessageContent = styled.main<{
+    isMine?: boolean;
+    isDeleted: boolean;
+}>`
+    margin-top: 0rem;
+    font-size: 0.85rem;
     color: ${({ isMine }) => (isMine ? 'white' : ' var(--gray-800)')};
+    opacity: ${({ isDeleted }) => (isDeleted ? 0.5 : 1)};
     word-break: break-all;
+    display: flex;
+    gap: 0.25rem;
+    align-items: center;
 `;
 
 export const MessageDate = styled.p<{ isMine: boolean }>`
     display: flex;
     justify-content: flex-end;
-    font-size: 0.65rem;
+    font-size: 0.6rem;
     color: ${({ isMine }) => (isMine ? 'white' : 'var(--gray-800)')};
     opacity: 0.85;
     user-select: none;
-
-    &:hover {
-        opacity: 1;
-    }
 `;
 
 export const OptionsButton = styled.button`
     color: white;
-    padding: 0.25rem;
-    transition: all 0.2s ease-in-out;
-    border-radius: 99rem;
-    &:hover {
-        background-color: rgba(255, 255, 255, 0.2);
-    }
+    padding: 0.5rem;
+    border-top-right-radius: 1rem;
+    background-color: var(--indigo-700);
 `;
