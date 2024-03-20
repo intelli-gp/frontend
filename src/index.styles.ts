a7b072cc6db87160121c42fd56be6a80cf1678b0
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-03-21 00:10:31 +0200

    feat: update global styling and vite config

--- src/index.styles.ts
+++ src/index.styles.ts
@@ -3,8 +3,9 @@ import styled, { css } from 'styled-components';
 export const PageTitle = styled.h1`
     user-select: none;
     line-height: 1.25;
-    font-weight: 900;
+    font-weight: 700;
     font-size: 2.5rem;
+    letter-spacing: -1.5px;
 
     @media (max-width: 768px) {
         line-height: 0.9;
@@ -16,6 +17,7 @@ export const ModalTitle = styled.h2<{ fontSize?: 'sm' | 'md' | 'lg' | string }>`
     user-select: none;
     font-weight: 700;
     line-height: 1.25;
+    letter-spacing: -1px;
     font-size: ${({ fontSize }) => {
         switch (fontSize) {
             case 'sm':
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
     server: {
         host: '0.0.0.0',
     },
