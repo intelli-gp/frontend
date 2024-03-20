122718bbf7b19160fbb247e3068a6df3742d23c0
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-03-20 02:03:09 +0200

    feat: update global styling

--- src/index.styles.ts
+++ src/index.styles.ts
@@ -64,5 +64,5 @@ export const BetweenPageAnimation = {
     initial: { opacity: 0 },
     animate: { opacity: 1 },
     exit: { opacity: 0 },
-    transition: { duration: 0.5 },
+    transition: { duration: 0.4 },
 };
