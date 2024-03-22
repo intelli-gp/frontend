5fffcfe9e94e6a4deff8c0db4f79afacb542a9b4
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-03-22 05:27:41 +0200

    feat: add not found page

---
+++ src/pages/not-found-page/not-found.styles.ts
@@ -0,0 +1,23 @@
+import { motion } from "framer-motion";
+import { styled } from "styled-components";
+
+export const PageContainer = styled(motion.div)`
+display: flex;
+
+    flex-direction: row;
+    height: 100vh;
+    align-items: center;
+    justify-content: space-between;
+    background: var(--indigo-50);
+    padding: 6.5rem;
+     
+    @media (max-width: 768px) {
+        padding: 3rem;
+    }
+`;
+export const Title = styled.h1`
+font-size: 3.2rem;
+font-weight: bold;
+color: var(--indigo-950);
+`
+
