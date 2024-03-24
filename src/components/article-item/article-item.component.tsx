6b8202e8ab9e80cc8a6b681a43582ea203725558
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-03-25 00:58:53 +0200

    fix: add paragraph part to article item

--- src/components/article-item/article-item.component.tsx
+++ src/components/article-item/article-item.component.tsx
@@ -1,7 +1,7 @@
 import defaultUserImage from '../../assets/imgs/user.jpg';
 import { ReceivedArticle } from '../../types/article';
 import Tag from '../tag/tag.component';
-import { Container, Title, UserImage, Username } from './article-item.style';
+import { Container, Graph, Title, UserImage, Username,ArticleImg } from './article-item.style';
 
 const SingleBlog = ({
     Author: author,
@@ -9,22 +9,23 @@ const SingleBlog = ({
     ArticleTags: tags,
     Title: title,
     UpdatedAt: updatedAt,
+    Sections:sections,
 }: ReceivedArticle) => {
     return (
         <Container>
-            <img
+            <ArticleImg
                 src={coverImageUrl}
                 alt="blog img"
-                className="w-full rounded-t-xl"
             />
-            <div className="py-[12px] px-[20px]">
-                <div className="flex flex-col gap-2">
+            <div className="py-[12px] px-[20px] h-[59%]">
+                <div className="flex flex-col h-full">
                     <div className="flex flex-row p-[8px] gap-2">
                         {tags
                             ?.slice(0, 3)
                             .map((tag) => <Tag text={tag} size="sm" />)}
                     </div>
-                    <div className="flex  justify-between">
+                    <span className='flex-1 py-2 flex flex-col gap-[6px]'>
+                    <div className="flex  justify-between ">
                         <Title>{title}</Title>
                         <p className="text-xs  text-slate-600 pt-2 ">
                             {new Date(updatedAt).toLocaleDateString('en-US', {
@@ -34,6 +35,11 @@ const SingleBlog = ({
                             })}
                         </p>
                     </div>
+                    <Graph>
+                    {sections[0].Value}
+                    </Graph>
+                    </span>
+                    <span className='flex flex-col gap-[7px]'>
                     <hr className="my-0 border-slate-200" />
                     <div className="flex items-center ">
                         <UserImage
@@ -42,6 +48,7 @@ const SingleBlog = ({
                         />
                         <Username>{author.FullName}</Username>
                     </div>
+                    </span>
                 </div>
             </div>
         </Container>
--- src/components/article-item/article-item.style.ts
+++ src/components/article-item/article-item.style.ts
@@ -8,12 +8,19 @@ export const Title = styled.h1`
     font-weight: bold;
     max-width: 70%;
     overflow: hidden;
-    white-space: wrap;
-    text-overflow: ellipsis;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    display: -webkit-box;
 `;
-
+export const ArticleImg = styled.img`
+  width: 100%;
+  border-radius:  0.75rem 0.75rem 0 0;
+  height: 45%;
+  box-shadow: 0 4px 6px 0 rgba(31, 41, 55, 0.1);
+`
 export const Container = styled.div`
     width: 100%;
+    height:80%;
     transition: transform 0.2s;
     border-radius: 0.75rem 0.75rem 0 0;
     display: flex;
@@ -45,3 +52,11 @@ export const Username = styled.p`
     text-transform: capitalize;
     color: var(--slate-600);
 `;
+export const Graph =styled.p`
+font-size: 1rem;
+overflow: hidden;
+-webkit-box-orient: vertical;
+-webkit-line-clamp: 3;
+display: -webkit-box;
+
+`
