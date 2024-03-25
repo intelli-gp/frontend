ea75b041a9b47bb634241015c480ffc2ad0642db
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-03-25 06:08:09 +0200

    feat: initialize the chat page component

---
+++ src/pages/chats/chats.page.tsx
@@ -0,0 +1,9 @@
+import { PageContainer,Title } from "./chats.styles"
+
+
+export const ChatsPage =()=>{
+    return <PageContainer>
+    <Title> Chats</Title>
+        
+    </PageContainer>
+}
---
+++ src/pages/chats/chats.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const PageContainer = styled(motion.div)`
+padding: 20px 30px;
+
+`
+
+export const Title =styled.div`
+    font-size: 2rem;
+    font-weight: bold;
+    color: var(--indigo-950);
+
+`
