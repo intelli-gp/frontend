b1238407100ef9345d7b713257212f56d3d441c6
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-03-26 17:03:47 +0200

    feat: create chat card component for chats page

---
+++ src/components/chat-card/chat-card.component.tsx
@@ -0,0 +1,42 @@
+import { CardContainer, CardImage, CardImageContainer, ChatContent, ChatDate, GroupTitle, TypographyContainer } from "./chat-card.style";
+import { ReceivedGroup } from "../../types/group";
+import defaultGroupImage from '../../assets/imgs/default-group-image.jpg';
+import { useNavigate } from "react-router-dom";
+
+
+
+const ChatCard = ({
+    ID,
+    GroupTitle: title,
+    GroupCoverImage,
+  }: Partial<ReceivedGroup>) => {
+    const navigate = useNavigate();
+    const message ={
+        Username:'Youmna_Mahmoud',
+        Content:'I am hosting a party this Thursday at 4 PM and I would love for all of you to join me. I am confident that you will have a great time. If you plan on attending, please kindly confirm your presence.'  }
+ 
+return <CardContainer
+        onClick={() => {
+            navigate(`/app/chat-room/${ID}`);
+        }}
+    >
+        <CardImageContainer>
+            <CardImage
+                src={GroupCoverImage || defaultGroupImage}
+                alt={title}
+            />
+        </CardImageContainer>
+        <TypographyContainer>
+        <GroupTitle title={title}>{title}</GroupTitle>
+       <ChatContent>
+        <span className="font-extrabold">{message.Username+': '}</span>
+        {message.Content}
+       </ChatContent>
+        <ChatDate>
+            3 minutes ago
+        </ChatDate>
+        </TypographyContainer>
+    </CardContainer>
+}
+
+export default ChatCard;
---
+++ src/components/chat-card/chat-card.style.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import { CSSTextLinesCountLimit } from "../../index.styles";
+
+
+export const CardImageContainer = styled.div`
+    height: 80px;
+    position: relative;
+`;
+
+export const CardContainer = styled.div`
+    width: 75%;
+    height:140px;
+    gap: 0.5rem;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    flex-direction:row;
+    padding: 1.25rem;
+    border-radius: 10px;
+    background-color: white;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    transition: all 0.25s ease-in-out;
+    cursor: pointer;
+    &:hover {
+        background-color: var(--indigo-25);
+    }
+`;
+
+export const CardImage = styled.img`
+    max-width: 80px;
+    border-radius: 50%;
+    object-fit: cover;
+    border: none;
+    aspect-ratio: 1/1;
+    @media (max-width: 768px) {
+        max-width: 100px;
+    }
+`;
+export const TypographyContainer = styled.div`
+   display:flex;
+   flex-direction:column;
+   align-items: flex-start;
+   padding-left:10px;
+   justify-content: space-between;
+   margin-left: 0.25rem;
+gap:6px;
+`;
+export const ChatDate = styled.time`
+    font-size: 0.75rem;
+    opacity: 0.9;
+    @media (max-width: 768px) {
+        font-size: 0.7rem;
+    }
+`;
+export const ChatContent = styled.p`
+    font-size: 0.8rem;
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    display: -webkit-box;
+`;
+export const GroupTitle = styled.h3<{ width?: string; lines?: number }>`
+${CSSTextLinesCountLimit}
+font-weight: bold;
+font-size: 1.25rem;
+
+`;
