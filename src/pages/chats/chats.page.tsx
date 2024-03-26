3b9eadefad57f40ca381431fe7f0667333f9d12c
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-03-26 17:11:43 +0200

    feat(ChatsPage): add ExploreHeader and ChatCard

--- src/pages/chats/chats.page.tsx
+++ src/pages/chats/chats.page.tsx
@@ -1,9 +1,38 @@
-import { PageContainer,Title } from "./chats.styles"
+import { useEffect, useState } from "react";
+import { PageContainer, Title } from "./chats.styles"
+import ExplorePageHeader from "../../components/explore-page-header/explore-page-header.component";
+import { useGetUserGroupsQuery } from "../../store";
+import { ReceivedGroup } from "../../types/group";
+import { Response } from '../../types/response';
+import ChatCard from "../../components/chat-card/chat-card.component";
 
 
-export const ChatsPage =()=>{
+export const ChatsPage = () => {
+    const [searchValue, setSearchValue] = useState('');
+    const handleChangeSearchValue = (value: string) => {
+        setSearchValue(value);
+    };
+    const [userGroups, setUserGroups] = useState<Partial<ReceivedGroup>[]>([]);
+    const { data: _groups } = useGetUserGroupsQuery();
+    const Groups: ReceivedGroup[] = (_groups as unknown as Response)?.data ?? [];
+    useEffect(() => {
+        setUserGroups(Groups);
+    }, [Groups]);
+
     return <PageContainer>
-    <Title> Chats</Title>
-        
+        <Title> Chats</Title>
+        <ExplorePageHeader
+            placeholder="Search chats..."
+            searchValue={searchValue}
+            onSearchValueChange={handleChangeSearchValue}
+            WithoutButton
+        />
+        {
+            userGroups.map((group) => {
+                return (
+                    <ChatCard {...group} />
+                )
+            })
+        }
     </PageContainer>
 }
--- src/pages/chats/chats.styles.ts
+++ src/pages/chats/chats.styles.ts
@@ -2,12 +2,17 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 export const PageContainer = styled(motion.div)`
-padding: 20px 30px;
-
+margin: 0 auto;
+padding: 2rem;
+max-width: 800px;
+display: flex;
+flex-direction: column;
+gap: 2rem;
+align-items: start;
 `
 
 export const Title =styled.div`
-    font-size: 2rem;
+    font-size: 3.25rem;
     font-weight: bold;
     color: var(--indigo-950);
 
