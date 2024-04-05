a2a7786981fd9f62a3f9c04cd30a6c10be0fb898
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-04-05 17:19:18 +0200

    chore: update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    optimizeDeps: {
+        exclude: ['js-big-decimal'],
+    },
     server: {
         host: '0.0.0.0',
     },
