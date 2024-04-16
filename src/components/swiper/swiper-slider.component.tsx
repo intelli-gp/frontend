deb1ad0c462a33b059de12306552b1e45bb3f5a6
Omar <ofahmy1234@gmail.com>
2024-04-16 09:48:54 +0200

    feat(courses): add custom slider to be used with different children

---
+++ src/components/swiper/swiper-slider.component.tsx
@@ -0,0 +1,37 @@
+import { isArray } from 'lodash';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { SwiperSlide } from 'swiper/react';
+
+import { SwiperSliderContainer } from './swiper-slider.styles';
+
+export type SwiperSliderProps = {
+    children: React.ReactNode | React.ReactNode[];
+};
+
+export const SwiperSlider = ({ children }: SwiperSliderProps) => {
+    return (
+            <SwiperSliderContainer
+                navigation={true}
+                modules={[Navigation]}
+                spaceBetween={15}
+                slidesPerView={'auto'}
+            >
+                {isArray(children) ? (
+                    (children as React.ReactNode[])?.map((child, index) => (
+                        <SwiperSlide
+                            style={{ width: 'auto' }}
+                            key={`carousel-child-${index}`}
+                        >
+                            {child}
+                        </SwiperSlide>
+                    ))
+                ) : (
+                    <SwiperSlide style={{ width: 'auto' }}>
+                        {children}
+                    </SwiperSlide>
+                )}
+            </SwiperSliderContainer>
+    );
+};
