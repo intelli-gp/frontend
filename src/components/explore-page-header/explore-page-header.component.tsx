5cf9e46fdb35aa1df18f2ed4d293aee797f96d02
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-04-17 16:30:25 +0200

    feat: update explore-page-header component
    add embedded autocomplete functionality to the component

--- src/components/explore-page-header/explore-page-header.component.tsx
+++ src/components/explore-page-header/explore-page-header.component.tsx
@@ -1,10 +1,15 @@
-import { FaPlus } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import { CgMathPlus } from 'react-icons/cg';
 
+import { useLazyGetAutocompleteSuggestionsQuery } from '../../store/apis/searchApi';
+import { SUGGESTION_TYPE } from '../../types/search';
 import {
     Container,
     CreateButton,
     SearchBarContainer,
     SearchIcon,
+    SuggestionItem,
+    SuggestionsContainer,
 } from './explore-page-header.style';
 
 type ExplorePageHeaderProps = {
@@ -18,7 +23,12 @@ type ExplorePageHeaderProps = {
      * icon is clicked or user press `Enter`
      * after typing on the search bar
      */
-    searchHandler?: () => void | Promise<void>;
+    searchHandler?: (searchTerm: string) => void | Promise<void>;
+    /**
+     * If passed, the search suggestions will be shown
+     * when the user types on the search bar.
+     */
+    suggestionsType?: SUGGESTION_TYPE;
 };
 
 const ExplorePageHeader = ({
@@ -28,12 +38,54 @@ const ExplorePageHeader = ({
     placeholder,
     WithoutButton,
     searchHandler,
+    suggestionsType,
 }: ExplorePageHeaderProps) => {
+    const [showSuggestions, setShowSuggestions] = useState(false);
+
+    const [getAutoCompleteSuggestions, { data: _searchSuggestions }] =
+        useLazyGetAutocompleteSuggestionsQuery();
+    const searchSuggestions = _searchSuggestions?.data as string[];
+
     const handleKeyDown = (e: React.KeyboardEvent) => {
         if (e.key === `Enter`) {
-            searchHandler && searchHandler();
+            searchHandler && searchHandler(searchValue);
         }
     };
+
+    const handleSearchValueChange = async (
+        e: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        onSearchValueChange(e.target.value);
+        if (!suggestionsType) return;
+        await getAutoCompleteSuggestions({
+            searchTerm: e.target.value,
+            type: suggestionsType,
+        });
+    };
+
+    const handleClickSuggestion = (suggestion: string) => {
+        onSearchValueChange(suggestion);
+        searchHandler && searchHandler(suggestion);
+        setShowSuggestions(false);
+    };
+
+    useEffect(() => {
+        const screenClickHandler = () => {
+            setShowSuggestions(false);
+        };
+
+        document.addEventListener('click', screenClickHandler);
+
+        return () => {
+            document.removeEventListener('click', screenClickHandler);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (searchSuggestions?.length) {
+            setShowSuggestions(true);
+        } else setShowSuggestions(false);
+    }, [searchSuggestions]);
 
     return (
         <Container onKeyDown={handleKeyDown}>
@@ -41,21 +93,51 @@ const ExplorePageHeader = ({
                 <SearchIcon
                     title={'Search'}
                     onClick={() => {
-                        searchHandler && searchHandler();
+                        searchHandler && searchHandler(searchValue);
                     }}
                 />
                 <input
                     className="border-none outline-none focus-visible:outline-none flex-1"
                     placeholder={placeholder ?? 'Search...'}
                     value={searchValue}
-                    onChange={(e) => onSearchValueChange(e.target.value)}
+                    onChange={handleSearchValueChange}
                 />
+                {showSuggestions && (
+                    <SuggestionsContainer
+                        initial={{
+                            height: 0,
+                        }}
+                        animate={{
+                            height: 'auto',
+                        }}
+                    >
+                        {searchSuggestions?.map((suggestion) => (
+                            <SuggestionItem
+                                onClick={() =>
+                                    handleClickSuggestion(suggestion)
+                                }
+                            >
+                                {suggestion}
+                            </SuggestionItem>
+                        ))}
+                    </SuggestionsContainer>
+                )}
             </SearchBarContainer>
             {WithoutButton ? (
                 <></>
             ) : (
-                <CreateButton select="primary200" onClick={onCreateButtonClick}>
-                    <FaPlus size={12} className="mr-1" /> Create
+                <CreateButton
+                    title="Create"
+                    onClick={onCreateButtonClick}
+                    whileHover={{
+                        rotate: 90,
+                        scale: 1.075,
+                        transition: {
+                            duration: 0.25,
+                        },
+                    }}
+                >
+                    <CgMathPlus size={24} />
                 </CreateButton>
             )}
         </Container>
--- src/components/explore-page-header/explore-page-header.style.ts
+++ src/components/explore-page-header/explore-page-header.style.ts
@@ -1,7 +1,19 @@
+import { motion } from 'framer-motion';
 import { LuSearch } from 'react-icons/lu';
 import styled from 'styled-components';
 
-import Button from '../button/button.component';
+import { CSSTextLengthLimit } from '../../index.styles';
+
+export const Container = styled.div`
+    background-color: transparent;
+    display: flex;
+    width: 100%;
+    max-width: 750px;
+    min-width: 0px;
+    gap: 0.5rem;
+    justify-content: center;
+    align-items: center;
+`;
 
 export const SearchBarContainer = styled.div`
     width: 100%;
@@ -14,23 +26,60 @@ export const SearchBarContainer = styled.div`
     padding-left: 0.25rem;
     gap: 0.5rem;
     box-shadow: 0 0 20px 10px rgb(99, 102, 241, 0.075);
+    position: relative;
 `;
 
-export const CreateButton = styled(Button)`
-    border-radius: 0.5rem;
+export const CreateButton = styled(motion.button)`
+    border-radius: 50%;
     border: none;
-    box-shadow: 0 0 20px 10px rgb(99, 102, 241, 0.075);
-`;
-
-export const Container = styled.div`
-    background-color: transparent;
+    width: 40px;
+    height: 40px;
+    aspect-ratio: 1/1;
+    box-sizing: content-box;
+    color: var(--indigo-900);
     display: flex;
-    width: 100%;
-    max-width: 750px;
-    min-width: 0px;
-    gap: 0.5rem;
     justify-content: center;
     align-items: center;
+    padding: 0;
+    background-color: var(--indigo-200);
+    transition: filter 0.15s ease-in-out;
+    svg {
+        stroke-width: 0.5;
+    }
+`;
+
+export const SuggestionsContainer = styled(motion.ul)`
+    position: absolute;
+    width: calc(100% - 2rem);
+    top: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    z-index: 10;
+    box-shadow: 0 0 20px 10px rgb(99, 102, 241, 0.075);
+    border-bottom-left-radius: 0.5rem;
+    border-bottom-right-radius: 0.5rem;
+    border: 1px solid var(--gray-300);
+    overflow: hidden;
+`;
+
+export const SuggestionItem = styled(motion.li)<{ width?: string }>`
+    transition: background-color 0.15s ease-in-out;
+    display: block;
+    cursor: pointer;
+    padding: 0.25rem 0.75rem;
+    margin: 0.125rem 0.5rem;
+    border-radius: 0.25rem;
+    &:hover {
+        background-color: var(--indigo-50);
+    }
+    &:first-child {
+        margin-top: 0.5rem;
+    }
+    &:last-child {
+        margin-bottom: 0.5rem;
+    }
+    ${CSSTextLengthLimit};
 `;
 
 export const SearchIcon = styled(LuSearch)`
