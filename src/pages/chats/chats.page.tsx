cf72d7b3fe19e8f1ab48b610b11423d568da0354
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-04-19 23:20:32 +0200

    refactor: ChatPage to include search functionality

--- src/pages/chats/chats.page.tsx
+++ src/pages/chats/chats.page.tsx
@@ -1,23 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import Skeleton from '../../components/Skeleton';
 import ChatCard from '../../components/chat-card/chat-card.component';
 import ExplorePageHeader from '../../components/explore-page-header/explore-page-header.component';
-import { useFetchMessagesQuery } from '../../store';
-import { MessagesNotification } from '../../types/notifications';
+import { useFetchMessagesQuery, } from '../../store';
 import { PageContainer, Title } from './chats.styles';
+import { MessagesNotification } from '../../types/notifications';
+import Skeleton from '../../components/Skeleton';
+import Fuse from 'fuse.js';
 
 export const ChatsPage = () => {
     const [searchValue, setSearchValue] = useState('');
+
+
+    const { data: _groups, isLoading, error } = useFetchMessagesQuery(undefined);
+    const data = _groups || [];
+
+    const [groups, setGroups] = useState(data);
+    useEffect(() => {
+        setGroups(data);
+    }, [data]);
     const handleChangeSearchValue = (value: string) => {
         setSearchValue(value);
+        const fuseOptions = {
+            keys: ['Group.GroupTitle'],
+            includeScore: true,
+        };
+        const fuse = new Fuse(data, fuseOptions);
+        const results = fuse.search(searchValue);
+        const filteredSearch =
+            value === '' ? data : results.map((result) => result.item);
+        setGroups(filteredSearch);
     };
-
-    const {
-        data: _groups,
-        isLoading,
-        error,
-    } = useFetchMessagesQuery(undefined);
 
     return (
         <PageContainer>
@@ -29,15 +42,13 @@ export const ChatsPage = () => {
                 WithoutButton
             />
             {isLoading ? (
-                <div className="h-auto w-full">
-                    <Skeleton times={3} className="h-24 w-full" />
-                </div>
+              <div className="h-auto w-full flex justify-center flex-col items-center gap-2">
+              <Skeleton times={3} className="h-[120px] w-[80%]" />
+          </div>    
             ) : error ? (
                 <div>Error</div>
-            ) : _groups?.length || 0 > 0 ? (
-                _groups?.map((group: Partial<MessagesNotification>) => (
-                    <ChatCard {...group} />
-                ))
+            ) : groups?.length || 0 > 0 ? (
+                groups?.map((group: Partial<MessagesNotification>) => <ChatCard {...group} />)
             ) : (
                 <div>No groups found.</div>
             )}
