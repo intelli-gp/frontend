b576213fb7d14a380ddb5603f068a49ab2adf79d
Omar <ofahmy1234@gmail.com>
2024-04-20 10:20:59 +0200

    feat(courses): add course results grid for expanded courses view or search results

---
+++ src/components/course-results-grid/course-results-grid.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const CourseSearchResultsGridContainer = styled.div`
+    min-width: 70%;
+    max-width: 70%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 320px);
+    gap: 1.5rem;
+    place-items: center;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 2rem;
+`;
+
