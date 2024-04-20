325301883293c8e522546ab353118e972324c417
Omar <ofahmy1234@gmail.com>
2024-04-20 10:11:01 +0200

    feat(core): add search not found component

---
+++ src/components/search-not-found/search-not-found.styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+
+export const SearchNotFoundContainer = styled.div`
+    /* width: 100%; */
+    /* height: 100%; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* border: 1px solid red; */
+`;
+
+export const SearchNotFoundImage = styled.img`
+    width: 70%;
+`;
+
+export const SearchNotFoundText = styled.h1`
+    font-size: 2rem;
+    font-weight: 500;
+    color: var(--gray-);
+`;
+
+export const SearchNotFoundSubText = styled.h2`
+    font-size: 1.5rem;
+    font-weight: 400;
+    color: var(--gray-500);
+`;
