69f9da2c2515bf3b0a178cd5049ccd7cacecf7d9
Omar <ofahmy1234@gmail.com>
2024-04-20 10:17:14 +0200

    feat(course): course section
    - add course preview section for explore courses with swiper

--- src/components/course-section/course-section.styles.ts
+++ src/components/course-section/course-section.styles.ts
@@ -7,6 +7,24 @@ export const CourseSectionContainer = styled.section`
     gap: 1.5rem;
 `;
 
+export const CourseSectionHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+`;
+
 export const CourseSectionTitle = styled.h2`
     font-size: 2.2rem;
+`;
+
+export const RedirectText = styled.p`
+    text-decoration: underline;
+    color: var(--gray-800);
+    font-weight: bold;
+    cursor: pointer;
+    font-size: 1.1rem;
+    transition: opacity 0.2s ease-in-out;
+    &:hover {
+        opacity: 0.8;
+    }
 `;
