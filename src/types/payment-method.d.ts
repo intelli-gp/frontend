9088b8b1ffccaa54c82277c683f688530fbfc320
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-05-03 16:44:24 +0300

    feat: add type for the payment method

---
+++ src/types/payment-method.d.ts
@@ -0,0 +1,13 @@
+export type PaymentMethod ={
+    holderName:string;
+    cardId: string;
+    cardNumber:string;
+    expiryDate:string;
+}
+export type RecievePaymentMethod ={
+    ID:number;
+    holderName:string;
+    cardId: string;
+    cardNumber:string;
+    expiryDate:string;
+}
