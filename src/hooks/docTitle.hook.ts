9ce22f89060a23e275325e4f88ea07393e04e20f
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-05-10 18:35:50 +0300

    feat: add useDocumentTitle hook

---
+++ src/hooks/docTitle.hook.ts
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+
+export function useDocumentTitle(minutes:number, seconds:number, isRunning:boolean) {
+    useEffect(() => {
+        console.log("I AM HERE")
+      if (isRunning) {
+        document.title = ` ${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')} - Pomodoro`;
+      } else {
+        document.title = `Mujedd`
+      }
+    }, [minutes, seconds, isRunning]);
+  }
