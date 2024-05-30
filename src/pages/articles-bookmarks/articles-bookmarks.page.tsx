c113ad523bc424b7f23002931996b9532a112c3a
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-05-30 15:21:33 +0300

    feat: update articles bookmarks page

--- src/pages/articles-bookmarks/articles-bookmarks.page.tsx
+++ src/pages/articles-bookmarks/articles-bookmarks.page.tsx
@@ -1,9 +1,10 @@
 import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import EmptyPagePlaceholder from '../../components/empty-page-placeholder/empty-placeholder.component';
 import UpButton from '../../components/up-button/up-button.components';
 import WideArticleItem from '../../components/wide-article-item/wide-article-item.component';
-import { PageTitle } from '../../index.styles';
+import { BetweenPageAnimation, PageTitle } from '../../index.styles';
 import {
     // usePrefetch as prefetch,
     useGetBookmarkedArticlesQuery,
@@ -13,6 +14,7 @@ import {
     MainContent,
     PageContainer,
 } from '../explore-articles/explore-articles.styles';
+import { ContentWrapper } from './articles-bookmarks.styles';
 
 const ArticlesBookmarksPage = () => {
     let navigate = useNavigate();
@@ -23,27 +25,48 @@ const ArticlesBookmarksPage = () => {
     });
 
     // let prefetchBookmarks = prefetch('getBookmarkedArticles');
-    let articles = data?.data?.Results;
+    let articles = data?.data?.Results ?? [];
+
+    let PageContent = () => {
+        if (isLoading) {
+            return <ArticlesSkeleton />;
+        } else {
+            if (articles?.length === 0) {
+                return (
+                    <EmptyPagePlaceholder
+                        text="No bookmarks found, let's explore some articles!"
+                        variant="no-data"
+                        button={{
+                            text: 'Explore Articles',
+                            path: '/app/articles',
+                        }}
+                    />
+                );
+            } else if (articles?.length > 0) {
+                return (
+                    <MainContent>
+                        {articles?.map((article) => (
+                            <WideArticleItem
+                                key={article.ID}
+                                {...article}
+                                onClick={() =>
+                                    navigate(`/app/articles/${article.ID}`)
+                                }
+                            />
+                        ))}
+                    </MainContent>
+                );
+            }
+        }
+    };
 
     return (
-        <PageContainer>
+        <PageContainer className='h-full' {...BetweenPageAnimation}>
             <PageTitle ref={headerRef}>My Bookmarks</PageTitle>
+            <ContentWrapper>
+                <PageContent />
+            </ContentWrapper>
             <UpButton pageHeaderElement={headerRef.current!} />
-            {isLoading ? (
-                <ArticlesSkeleton />
-            ) : (
-                <MainContent>
-                    {articles?.map((article) => (
-                        <WideArticleItem
-                            key={article.ID}
-                            {...article}
-                            onClick={() =>
-                                navigate(`/app/articles/${article.ID}`)
-                            }
-                        />
-                    ))}
-                </MainContent>
-            )}
         </PageContainer>
     );
 };
---
+++ src/pages/articles-bookmarks/articles-bookmarks.styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.div`
+    flex-grow: 1;
+`
