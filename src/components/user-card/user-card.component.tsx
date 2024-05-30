6480ddf9c1587cab57d8b411de59b33559bbcf9b
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-05-30 15:07:37 +0300

    feat: update use card component
    use the newly added FollowButton component to
    handle following other users and change clicking
    card behavior to do nothing and clicking only the
    user full name navigate to the user profile

--- src/components/user-card/user-card.component.tsx
+++ src/components/user-card/user-card.component.tsx
@@ -1,15 +1,8 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
-import {
-    RootState,
-    useGetFollowingQuery,
-    useToggleFollowUserMutation,
-} from '../../store';
+import { RootState } from '../../store';
 import { ReceivedUser } from '../../types/user';
 import { profileURL } from '../../utils/profileUrlBuilder';
-import { errorToast, successToast } from '../../utils/toasts';
 import {
     CardContainer,
     CardMainButton,
@@ -24,77 +17,21 @@ const UserCard = ({
     ProfileImage,
     ID,
 }: Partial<ReceivedUser>) => {
-    const navigate = useNavigate();
     const storedUser = useSelector((state: RootState) => state.auth.user);
 
     const isMe = storedUser.Username === Username;
 
-    const [toggleFollowUser, { isLoading: toggleFollowUserIsLoading }] =
-        useToggleFollowUserMutation();
-    const { data: _following } = useGetFollowingQuery(storedUser.ID!);
-    const loggedInUserFollowings = _following?.data?.Results as ReceivedUser[];
-
-    const alreadyFollowing = loggedInUserFollowings?.some(
-        (user) => user.Username === Username,
-    );
-
-    const handleToggleFollowUser = async () => {
-        try {
-            await toggleFollowUser(ID!).unwrap();
-            if (alreadyFollowing) {
-                successToast('User unfollowed successfully');
-            } else {
-                successToast('User followed successfully');
-            }
-        } catch (error) {
-            errorToast('Error following user');
-            console.log(error);
-        }
-    };
-
-    const handleNavigateToProfile = () => {
-        navigate('/app/profile');
-    };
-
-    const buttonTitle = ((alreadyFollowing: boolean, isMe: boolean) => {
-        if (isMe) return 'You cannot follow yourself';
-        if (alreadyFollowing) return 'Unfollow user';
-        return 'Follow user';
-    })(alreadyFollowing, isMe);
-
-    const buttonText = ((alreadyFollowing: boolean, isMe: boolean) => {
-        if (isMe) return 'View profile';
-        if (alreadyFollowing) return 'Unfollow';
-        return 'Follow';
-    })(alreadyFollowing, isMe);
-
-    const buttonVariant = ((alreadyFollowing: boolean, isMe: boolean) => {
-        if (isMe) return 'primary200';
-        if (alreadyFollowing) return 'danger';
-        return 'secondary';
-    })(alreadyFollowing, isMe);
-
-    const buttonOnClick = isMe
-        ? handleNavigateToProfile
-        : handleToggleFollowUser;
-
     return (
-        <CardContainer to={profileURL(Username!)}>
+        <CardContainer>
             <UserImage src={ProfileImage} alt="user profile image" />
-            <UserFullName>{isMe ? 'You' : FullName}</UserFullName>
+            <UserFullName to={profileURL(Username!)}>
+                {isMe ? 'You' : FullName}
+            </UserFullName>
             <UserUsername>@{Username}</UserUsername>
             <CardMainButton
-                loading={toggleFollowUserIsLoading}
-                onClick={buttonOnClick}
-                title={buttonTitle}
-                select={buttonVariant}
-                outline={alreadyFollowing}
-                onClickCapture={(e: React.MouseEvent) => {
-                    e.nativeEvent.preventDefault();
-                }}
-            >
-                {buttonText}
-            </CardMainButton>
+                AnotherUserUserName={Username!}
+                AnotherUserID={ID}
+            />
         </CardContainer>
     );
 };
--- src/components/user-card/user-card.styles.ts
+++ src/components/user-card/user-card.styles.ts
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { SwiperSlide } from 'swiper/react';
 
 import { CSSTextLengthLimit } from '../../index.styles';
-import Button from '../button/button.component';
+import { FollowButton } from '../follow-button/follow-button.component';
 import EnhancedImage from '../image/image.component';
 
 const CARD_WIDTH = '225px';
 
-export const CardContainer = styled(Link)`
+export const CardContainer = styled.div`
+    width: 90%;
     color: inherit;
     display: flex;
     flex-direction: column;
@@ -17,10 +17,9 @@ export const CardContainer = styled(Link)`
     gap: 0.75rem;
     padding: 1.5rem;
     border-radius: 2rem;
-    box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px 0px;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 8px 2px;
     transition: all 0.2s ease-in-out;
     width: ${CARD_WIDTH};
-    cursor: pointer;
     &:hover {
         background-color: var(--indigo-25);
     }
@@ -33,12 +32,18 @@ export const UserImage = styled(EnhancedImage)`
     aspect-ratio: 1/1;
 `;
 
-export const UserFullName = styled.p<{ width?: string }>`
+export const UserFullName = styled(Link)<{ width?: string }>`
     font-size: 1.25rem;
     font-weight: bold;
     text-align: center;
     line-height: 1;
+    color: inherit;
+    display: block;
+    text-decoration-thickness: 1px;
     ${CSSTextLengthLimit}
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 export const UserUsername = styled.p<{ width?: string }>`
@@ -50,16 +55,12 @@ export const UserUsername = styled.p<{ width?: string }>`
     ${CSSTextLengthLimit}
 `;
 
-export const CardMainButton = styled(Button)`
-    border-radius: 5rem;
+export const CardMainButton = styled(FollowButton)`
+    border-radius: 0.5rem;
     padding: 0.25rem 0;
-    width: 90%;
+    width: 75%;
     margin: 0 auto;
     font-size: 0.875rem;
     margin-top: 0.75rem;
     height: 2rem;
-`;
-
-export const SwiperCustomSlide = styled(SwiperSlide)<{ width?: string }>`
-    width: ${({ width }) => width ?? CARD_WIDTH} !important;
 `;
