80815eddfaed47704cef68ca2aaa97d7c2336482
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-06-21 13:37:43 +0300

    feat: update the AI service page

--- src/pages/ai-service/ai-service.page.tsx
+++ src/pages/ai-service/ai-service.page.tsx
@@ -1,6 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { PageContainer, SearchBarContainer, AttachmentIcon, GenerateButton, Holder, DownloadIcon, ShareIcon } from './ai-service.styles';
+import { BetweenPageAnimation, PageTitle } from '../../index.styles';
 
 export const AIServicePage = () => {
+    const [prompt, setPrompt] = useState('');
+
     useEffect(() => {
         document.title = 'AI Service | Mujedd';
         return () => {
@@ -8,5 +12,29 @@ export const AIServicePage = () => {
         };
     }, []);
 
-    return <div>AIServicePage</div>;
+    // const { data: subscriptionResponse, isLoading: isSubscriptionDataLoading, isError } = useGetSubscriptionQuery();
+    // const subscriptionData = subscriptionResponse?.data;
+
+    return (
+
+            <PageContainer {...BetweenPageAnimation}>
+                <PageTitle className="text-center">AI Service</PageTitle>
+                <SearchBarContainer>
+                    <AttachmentIcon size={18} />
+                    <input
+                        className="border-none outline-none focus-visible:outline-none flex-1"
+                        placeholder="Enter Your Prompt here..."
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}
+                    />
+                    <GenerateButton>Generate</GenerateButton>
+                </SearchBarContainer>
+                <Holder>
+                    <div className="flex w-full justify-end gap-2 items-center p-4">
+                        <ShareIcon size={20} title="Share" />
+                        <DownloadIcon size={20} title="Download" />
+                    </div>
+                </Holder>
+            </PageContainer>
+        );
 };
--- src/pages/ai-service/ai-service.styles.ts
+++ src/pages/ai-service/ai-service.styles.ts
@@ -1,1 +1,100 @@
-// import styled from "styled-components";
+
+import { motion } from 'framer-motion';
+import { GrAttachment } from "react-icons/gr";
+import styled, { css } from 'styled-components';
+import Button from '../../components/button/button.component';
+import { IoShareSocial } from "react-icons/io5";
+import { GoDownload } from "react-icons/go";
+export const PageContainer = styled(motion.div)`
+    margin: 0 auto;
+    padding: 2rem;
+    max-width: 800px;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    height:calc(100vh - 56px);
+    @media (max-width: 768px) {
+        padding: 2rem 1rem;
+    }
+
+`;
+
+export const SearchBarContainer = styled.div`
+    width: 100%;
+    border-radius: 5rem;
+    border: 1px solid var(--gray-300);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    box-shadow: 0 0 20px 10px rgb(99, 102, 241, 0.075);
+    position: relative;
+        z-index: 1;
+
+`;
+
+
+export const Background = styled(motion.div)`
+    height: 100%;
+    background: rgba(0, 0, 0, 0.75);
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 101;
+    width:100%;
+   
+`;
+export const AttachmentIcon = styled(GrAttachment)`
+    transition: all 0.2s ease-in-out;
+    box-sizing: content-box;
+    padding: 0.7rem 1rem;
+    color: var(--indigo-900);
+    cursor: pointer;
+    aspect-ratio: 1/1;
+    border-radius: 5rem 0rem 0rem 5rem ;
+    box-shadow: inset  -10px 0px 8px -6px rgb(99, 102, 241, 0.075);
+    background-color: var(--indigo-25);
+    transition: all 0.1s ease-in-out;  
+    &:hover {
+        background-color: var(--indigo-50);
+    }     
+`;
+export const GenerateButton = styled(Button)`
+    padding: 0.5rem 2.5rem;
+    color: var(--indigo-900);
+    border-radius: 0rem 5rem 5rem 0rem ;
+    background-color: var(--indigo-25);
+    border: none;
+    box-shadow: inset  10px 0px 6px -6px rgb(99, 102, 241, 0.075); 
+    transition: all 0.1s ease-in-out;  
+    &:hover {
+        background-color: var(--indigo-50);
+    }   
+`;
+export const Holder =styled.div`
+    width: 100%;
+    background-color: var(--indigo-25);
+    border-radius:6px;
+   flex:1;     
+
+`
+const commonFooterIconStyles = css`
+    color: var(--indigo-900);
+    box-sizing: content-box;
+    cursor: pointer;
+    transition: all 0.1s ease-in-out;
+    padding: 0.5rem;
+    border-radius: 50%;
+    &:hover {
+        color: var(--indigo-50);
+        background-color: var(--indigo-900);
+    }
+`;
+export const ShareIcon = styled(IoShareSocial)`
+    ${commonFooterIconStyles}
+`;
+
+export const DownloadIcon = styled(GoDownload)`
+    ${commonFooterIconStyles}
+`;
