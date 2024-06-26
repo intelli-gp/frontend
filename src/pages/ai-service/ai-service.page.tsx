015d86a99c2dfb6b1d29574bcd67fd2c5de5c11c
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-06-26 14:37:08 +0300

    feat: update ai service page styling

--- src/pages/ai-service/ai-service.page.tsx
+++ src/pages/ai-service/ai-service.page.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 
-import PlayImage from '../../assets/imgs/play-placeholder-2.svg';
+import PlayImage from '../../assets/imgs/vide-placeholder.svg';
 import { ForbiddenModal } from '../../components/ForbiddenModal';
-import Button from '../../components/button/button.component';
+import Skeleton from '../../components/Skeleton';
+import EnhancedImage from '../../components/image/image.component';
 import { Label } from '../../components/input/input.styles';
 import { BetweenPageAnimation, PageTitle } from '../../index.styles';
 import {
@@ -11,22 +12,40 @@ import {
 } from '../../store';
 import { errorToast, successToast } from '../../utils/toasts';
 import {
-    AttachmentIcon,
     GenerateButton,
-    Holder,
     PageContainer,
+    PromptInput,
     SearchBarContainer,
+    StarsIcon,
+    VideoWrapper,
 } from './ai-service.styles';
 
-const MainContent = ({ videoSrc }: { videoSrc: string }) => {
+type MainContentProps = {
+    videoSrc: string;
+    isLoading?: boolean;
+};
+const MainContent = ({ videoSrc, isLoading }: MainContentProps) => {
+    if (isLoading) {
+        return <Skeleton className="w-full h-full rounded-lg !m-0" darker />;
+    }
+
+    if (!videoSrc) {
+        return (
+            <div className="flex items-center justify-center w-full h-full">
+                <EnhancedImage
+                    src={PlayImage}
+                    alt="Play Image"
+                    className="rounded-lg"
+                />
+            </div>
+        );
+    }
+
     return (
         <video
-            width={'100%'}
-            height={500}
             src={videoSrc}
             controls
-            poster={PlayImage}
-            className="rounded-md"
+            className="rounded-[0.75rem] h-full object-contain"
         />
     );
 };
@@ -46,6 +65,7 @@ export const AIServicePage = () => {
     }, []);
 
     const handleGenerateVideo = async () => {
+        if (!prompt.trim()) return errorToast('Invalid empty prompt');
         try {
             const { data } = await generateVideo({ Content: prompt }).unwrap();
             setVideoSrc(data.Url);
@@ -57,6 +77,13 @@ export const AIServicePage = () => {
 
     const { error } = useTestSubscriptionQuery();
 
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            handleGenerateVideo();
+        }
+    };
+
     useEffect(() => {
         if ((error as any)?.status === 403) {
             setInvalidSubscription(true);
@@ -65,39 +92,42 @@ export const AIServicePage = () => {
 
     return (
         <PageContainer {...BetweenPageAnimation}>
-            <PageTitle className="text-center">AI Lectures</PageTitle>
-            <SearchBarContainer>
-                <AttachmentIcon size={18} />
-                <input
-                    className="border-none outline-none focus-visible:outline-none flex-1"
-                    placeholder="Enter Your Prompt here..."
-                    value={prompt}
-                    onChange={(e) => setPrompt(e.target.value)}
-                />
-                <GenerateButton
-                    onClick={handleGenerateVideo}
-                    loading={isLoading}
-                >
-                    Generate
-                </GenerateButton>
-            </SearchBarContainer>
-            <div className="mt-6 flex flex-col gap-4">
+            <PageTitle className="text-center">AI Lectures Generator</PageTitle>
+            <div className="flex flex-col gap-2">
+                <SearchBarContainer>
+                    <PromptInput
+                        placeholder="Enter Your Prompt here..."
+                        value={prompt}
+                        multiline
+                        row={3}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            setPrompt(e.target.value)
+                        }
+                        onKeyDown={handleKeyDown}
+                    />
+                </SearchBarContainer>
                 <div className="flex items-center gap-4">
-                    <Label className="!w-auto">Select your instructor:</Label>
-                    <select className="px-4 py-2 bg-indigo-100 rounded-md hover:bg-indigo-200 cursor-pointer">
+                    <Label className="!text-[1.25rem]">
+                        Select your instructor:
+                    </Label>
+                    <select className="!w-[140px] !h-[40px] bg-indigo-200 rounded-xl cursor-pointer px-2">
                         <option value="Andrew Ng">Andrew Ng</option>
                     </select>
+                    <GenerateButton
+                        select="primary200"
+                        onClick={handleGenerateVideo}
+                        loading={isLoading}
+                    >
+                        <StarsIcon />
+                        Generate
+                    </GenerateButton>
                 </div>
-                <Holder>
-                    <div className="flex-grow">
-                        <MainContent videoSrc={videoSrc} />
-                    </div>
-                    <div className="flex w-full gap-2 flex-row-reverse pb-4 px-4">
-                        <Button outline>Share</Button>
-                        <Button select="warning">Download</Button>
-                    </div>
-                </Holder>
             </div>
+
+            <VideoWrapper>
+                <MainContent videoSrc={videoSrc} />
+            </VideoWrapper>
+
             <ForbiddenModal
                 isOpen={invalidSubscription}
                 setIsOpen={setInvalidSubscription}
--- src/pages/ai-service/ai-service.styles.ts
+++ src/pages/ai-service/ai-service.styles.ts
@@ -3,6 +3,7 @@ import { BsStars } from 'react-icons/bs';
 import styled from 'styled-components';
 
 import Button from '../../components/button/button.component';
+import { CustomInput } from '../../components/input/Input.component';
 
 export const PageContainer = styled(motion.div)`
     margin: 0 auto;
@@ -10,8 +11,8 @@ export const PageContainer = styled(motion.div)`
     max-width: 1000px;
     display: flex;
     flex-direction: column;
-    gap: 1rem;
-    height: calc(100vh - 56px);
+    gap: 2rem;
+    height: calc(100vh - var(--mobile-nav-height));
     @media (max-width: 768px) {
         padding: 2rem 1rem;
     }
@@ -19,15 +20,19 @@ export const PageContainer = styled(motion.div)`
 
 export const SearchBarContainer = styled.div`
     width: 100%;
-    border-radius: 5rem;
-    border: 1px solid var(--gray-300);
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 0.5rem;
-    box-shadow: 0 0 20px 10px rgb(99, 102, 241, 0.075);
-    position: relative;
     z-index: 1;
+`;
+
+export const PromptInput = styled(CustomInput)`
+    border: none;
+    border: 1px solid var(--gray-500);
+    border-radius: 0.75rem;
+    padding-left: 1rem;
+    resize: none;
 `;
 
 export const Background = styled(motion.div)`
@@ -41,35 +46,29 @@ export const Background = styled(motion.div)`
     width: 100%;
 `;
 
-export const AttachmentIcon = styled(BsStars)`
+export const StarsIcon = styled(BsStars)`
     box-sizing: content-box;
-    padding: 0.7rem 1rem;
-    color: var(--indigo-900);
-    border-radius: 5rem 0rem 0rem 5rem;
-    transition: all 0.1s ease-in-out;
 `;
 
 export const GenerateButton = styled(Button)`
-    padding: 0.5rem 2rem;
-    padding-left: 1rem;
-    border-radius: 0rem 5rem 5rem 0rem;
-    background-color: var(--indigo-100);
-    color: inherit;
     border: none;
     height: 40px;
-    width: 120px;
-    box-shadow: inset 10px 0px 6px -6px rgb(99, 102, 241, 0.075);
-    transition: all 0.1s ease-in-out;
-    &:hover {
-        background-color: var(--indigo-200);
-    }
+    width: 140px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    border-radius: 0.75rem;
+    margin-left: auto;
 `;
-export const Holder = styled.div`
+
+export const VideoWrapper = styled.div`
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
     width: 100%;
-    background-color: var(--indigo-100);
-    border-radius: 6px;
-    flex-shrink: 1;
+    overflow: hidden;
+    border-radius: 0.75rem;
     display: flex;
     flex-direction: column;
-    gap: 1.5rem;
+    background-color: var(--indigo-950);
+    flex: 1;
 `;
